5da22149d18007293c3f3b542f32f3e683e9a602
hancock <hancock@ea017acb-4f8c-4593-beb0-474fb8108aa2>
2012-07-13 01:54:04 +0000

    the flags are now stored in a module of their own for easy access from codes outside of Aegean

---
+++ Imports/flags.py
@@ -0,0 +1,12 @@
+# Set some bitwise logic for flood routines
+PEAKED = 1    # 001
+QUEUED = 2    # 010
+VISITED = 4   # 100
+
+# Err Flags for fitting routines
+FITERRSMALL   = 1 #00001
+FITERR        = 2 #00010
+FIXED2PSF     = 4 #00100
+FIXEDCIRCULAR = 8 #01000
+NOTFIT        =16 #10000
+
