35e4e690d2640c391c430dd30475adb248a0c416
PaulHancock <paul.hancock@curtin.edu.au>
2014-10-03 17:07:39 +0800

    refactored code separated command line arguments into groups for clarity not supplying any options prints help enabled +/-p for polygons

--- MIMAS.py
+++ MIMAS.py
@@ -45,10 +45,11 @@ def maskfile(regionfile,infile,outfile):
         wcs = pywcs.WCS(im[0].header, naxis=2)
     except:
         wcs = pywcs.WCS(str(im[0].header),naxis=2)
-    data = im[0].data
+    data = np.squeeze(im[0].data)
 
     #easy/slow version
     #TODO: revise this to be faster if at all possible.
+    print data.shape
     for i,row in enumerate(data):
         for j,val in enumerate(row):
             skybox = wcs.wcs_pix2world([[i,j]],1)
@@ -69,45 +70,106 @@ def mim2reg(mimfile,regfile):
     return
 
 
+def combine_regions(container):
+    """
+    Return a region that is the combination of those specified in the container.
+    The container is typically a results instance that comes from argparse.
+    Any object with the properties [maxdepth,add_region,rem_region,include_circles, exclude_circles] will work
+    :param container: Object containing the region descriptions.
+    :return: A region
+    """
+    #create empty region
+    region=Region(container.maxdepth)
+
+    #add/rem all the regions from files
+    for r in container.add_region:
+        logging.info("adding region from {0}".format(r))
+        r2=pickle.load(r)
+        region.union(r2)
+
+    for r in container.rem_region:
+        logging.info("removing region from {0}".format(r))
+        r2=pickle.load(r)
+        region.without(r2)
+
+
+    #add circles
+    if len(container.include_circles) > 0:
+        ras,decs,radii=zip(*container.include_circles)
+        ras=map(np.radians, ras)
+        decs=map(np.radians, decs)
+        radii=map(np.radians, radii)
+        region.add_circles(ras, decs, radii)
+
+    #remove circles
+    if len(container.exclude_circles) > 0:
+        r2=Region(container.maxdepth)
+        ras,decs,radii=zip(*container.exclude_circles)
+        r2.add_circles(ras, decs, radii)
+        region.without(r2)
+    return region
+
+def save_region(region,filename):
+    """
+    Save the given region to a file
+    :param region: A Region
+    :param filename: A Filename
+    :return: None
+    """
+    pickle.dump(region,open(filename,'w'),protocol=-1)
+    logging.info("Wrote {0}".format(filename))
+    return
+
 if __name__=="__main__":
     epilog='Regions are added/subtracted in the following order, +r -r +c -c +p -p. This means that you might have to take multiple passes to construct overly complicated regions.'
     parser = argparse.ArgumentParser(epilog=epilog,prefix_chars='+-')
+
+    group1=parser.add_argument_group('Creating/modifying regions','Must specify -o, plus or more [+-][cr]')
     #tools for creating .mim files
-    parser.add_argument('-o', dest='outfile', action='store', help='output filename [default=region.mim]', default='region.mim')
-    parser.add_argument('-depth', dest='maxdepth',action='store',
+    group1.add_argument('-o', dest='outfile', action='store', help='output filename', default=None)
+    group1.add_argument('-depth', dest='maxdepth',action='store',
                         metavar='N', default=8, type=int,
                         help='maximum nside=2**N to be used to represent this region. [Default=8]')
-    parser.add_argument('+r', dest='add_region', action='append',
+    group1.add_argument('+r', dest='add_region', action='append',
                         default=[], type=str, metavar='filename',nargs='*',
                         help='add a region specified by the given file (.mim format)')
-    parser.add_argument('-r', dest='rem_region', action='append',
+    group1.add_argument('-r', dest='rem_region', action='append',
                         default=[], type=str, metavar='filename',nargs='*',
                         help='exclude a region specified by the given file ( .mim format)')
     #add/remove circles
-    parser.add_argument('+c', dest='include_circles', action='append',
+    group1.add_argument('+c', dest='include_circles', action='append',
                         default=[], type=float, metavar=('ra','dec','radius'), nargs=3,
                         help='add a circle to this region (decimal degrees)')
-    parser.add_argument('-c', dest='exclude_circles', action='append',
+    group1.add_argument('-c', dest='exclude_circles', action='append',
                         default=[],type=float, metavar=('ra','dec','radius'), nargs=3,
                         help='exclude the given circles from a region')
 
     #add/remove polygons
-    # parser.add_argument('+p', dest='included_polygons', action='append',
-    #                     default=[], type=float, metavar=('ra','dec'), nargs='*',
-    #                     help='add a polygon to this region ( decimal degrees)')
-    # parser.add_argument('-p', dest='excluded_polygons', action='append',
-    #                     default=[], type=float, metavar=('ra','dec'), nargs='*',
-    #                     help='remove a polygon from this region ( decimal degrees)')
+    group1.add_argument('+p', dest='included_polygons', action='append',
+                        default=[], type=float, metavar=('ra','dec'), nargs='*',
+                        help='add a polygon to this region ( decimal degrees)')
+    group1.add_argument('-p', dest='excluded_polygons', action='append',
+                        default=[], type=float, metavar=('ra','dec'), nargs='*',
+                        help='remove a polygon from this region ( decimal degrees)')
 
+    group2 = parser.add_argument_group("Using already created regions")
     #tools that use .mim files
-    parser.add_argument('--mim2reg',dest='mim2reg', action='append', type=str, metavar=('region.mim','region.reg'), nargs=2, help='convert region.mim into region.reg', default=[])
-    parser.add_argument('--mask',dest='mask', action='store',
+    group2.add_argument('--mim2reg',dest='mim2reg', action='append',
+                        type=str, metavar=('region.mim','region.reg'), nargs=2,
+                        help='convert region.mim into region.reg', default=[])
+    group2.add_argument('--mask',dest='mask', action='store',
                         type=str, metavar=('region.mim','file.fits','masked.fits'), nargs=3, default=[],
                         help='use region.mim to mask file.fits and write masekd.fits')
+    group3 = parser.add_argument_group('Extra options')
     #extras
-    parser.add_argument('--debug', dest='debug', action='store_true', help='debug mode [default=False]', default=False)
-    parser.add_argument('--version', action='version', version='%(prog)s '+version)
+    group3.add_argument('--debug', dest='debug', action='store_true', help='debug mode [default=False]', default=False)
+    group3.add_argument('--version', action='version', version='%(prog)s '+version)
     results = parser.parse_args()
+
+    #TODO: see if there is an 'argparse' way of detecting no input
+    if len(sys.argv)<=1:
+        parser.print_help()
+        sys.exit()
 
     logging_level = logging.DEBUG if results.debug else logging.INFO
     logging.basicConfig(level=logging_level, format="%(process)d:%(levelname)s %(message)s")
@@ -123,37 +185,6 @@ if __name__=="__main__":
         maskfile(m, i, o)
         sys.exit()
 
-    #create empty region
-    region=Region(results.maxdepth)
-
-    #add/rem all the regions from files
-    for r in results.add_region:
-        logging.info("adding region from {0}".format(r))
-        r2=pickle.load(r)
-        region.union(r2)
-
-    for r in results.rem_region:
-        logging.info("removing region from {0}".format(r))
-        r2=pickle.load(r)
-        region.without(r2)
-
-
-    #add circles
-    if len(results.include_circles) > 0:
-        ras,decs,radii=zip(*results.include_circles)
-        ras=map(np.radians, ras)
-        decs=map(np.radians, decs)
-        radii=map(np.radians, radii)
-        region.add_circles(ras, decs, radii)
-
-    #remove circles
-    if len(results.exclude_circles) > 0:
-        r2=Region(results.maxdepth)
-        ras,decs,radii=zip(*results.exclude_circles)
-        r2.add_circles(ras, decs, radii)
-        region.without(r2)
-
-    #write output
-    pickle.dump(region,open(results.outfile,'w'))
-    logging.info("Wrote {0}".format(results.outfile))
-
+    if results.outfile is not None:
+        region=combine_regions(results)
+        save_region(region,results.outfile)
