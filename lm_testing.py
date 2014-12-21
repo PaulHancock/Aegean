4e270f4d452335937401e28efcf10e1942863558
Paul Hancock <paul.hancock@curtin.edu.au>
2014-12-21 16:50:52 +0800

    added testing and profiling for floodfill replacement

--- lm_testing.py
+++ lm_testing.py
@@ -7,6 +7,127 @@ import sys
 from AegeanTools.mpfit import mpfit
 import logging
 
+from scipy.ndimage import label, find_objects
+import AegeanTools.flags as flags
+
+def explore(snr,status,queue,bounds,pixel):
+    """
+    Look for pixels adjacent to <pixel> and add them to the queue
+    Don't include pixels that are in the queue
+    :param snr: array of bool that is true for pixels we are interested in
+    :param status: array that represents the status of pixels
+    :param queue: the queue of pixels that we are interested in
+    :param bounds: the bounds within which we can explore
+    :param pixel: the initial point to start from
+    :return: None
+    """
+    (x, y) = pixel
+
+    if x > 0:
+        new = (x - 1, y)
+        if snr[new] and (not status[new] & flags.QUEUED):
+            queue.append(new)
+            status[new] |= flags.QUEUED
+
+    if x < bounds[0]:
+        new = (x + 1, y)
+        if snr[new] and (not status[new] & flags.QUEUED):
+            queue.append(new)
+            status[new] |= flags.QUEUED
+
+    if y > 0:
+        new = (x, y - 1)
+        if snr[new] and (not status[new] & flags.QUEUED):
+            queue.append(new)
+            status[new] |= flags.QUEUED
+
+    if y < bounds[1]:
+        new =  (x, y + 1)
+        if snr[new] and (not status[new] & flags.QUEUED):
+            queue.append(new)
+            status[new] |= flags.QUEUED
+
+def flood(snr,status,bounds,peak):
+    """
+    Start at pixel=peak and return all the pixels that belong to
+    the same blob.
+    :param snr: array of bool that is true for pixels we are interested in
+    :param status: array that represents the status of pixels
+    :param bounds: the bounds within which we can explore
+    :param peak: the initial point to start from
+    :return: None
+    """
+
+    if status[peak] & flags.VISITED:
+        return []
+
+    blob = []
+    queue = [peak]
+    status[peak] |= flags.QUEUED
+
+    for pixel in queue:
+        if status[pixel] & flags.VISITED:
+            continue
+
+        status[pixel] |= flags.VISITED
+
+        blob.append(pixel)
+        explore(snr, status, queue, bounds, pixel)
+
+    return blob
+
+def gen_flood_wrap(data,rmsimg,innerclip,outerclip=None,domask=False):
+    """
+    <a generator function>
+    Find all the sub islands in data.
+    Detect islands with innerclip.
+    Report islands with outerclip
+
+    type(data) = Island
+    return = [(pixels,xmin,ymin)[,(pixels,xmin,ymin)] ]
+    where xmin,ymin is the offset of the subisland
+    """
+    if outerclip is None:
+        outerclip=innerclip
+    #somehow this avoids problems with multiple cores not working properly!?!?
+    #TODO figure out why this is so.
+    abspix=abs(data)
+        
+    status=np.zeros(data.shape,dtype=np.uint8)
+    # Selecting PEAKED pixels
+
+    status[np.where(abspix/rmsimg>innerclip)] = flags.PEAKED
+
+    # making pixel list
+    ax,ay=np.where(abspix/rmsimg>innerclip)
+
+    #TODO: change this so that I can sort without having to decorate/undecorate
+    peaks=[(data[ax[i],ay[i]],ax[i],ay[i]) for i in range(len(ax))]
+
+    if len(peaks) == 0:
+        return
+
+    # sorting pixel list - strongest peak should be found first
+    peaks.sort(reverse = True)
+    if peaks[0][0] < 0:
+        peaks.reverse()
+    peaks=map(lambda x:x[1:],peaks) #strip the flux data so we are left with just the positions
+    bounds=(data.shape[0] - 1, data.shape[1] - 1)
+
+    snr = abspix/rmsimg >= outerclip
+    # starting image segmentation
+    for peak in peaks:
+        blob = flood(snr, status, bounds, peak)
+        npix=len(blob)
+        if npix>=1:
+            xmin = min(blob, key= lambda x:x[0])[0]
+            xmax = max(blob, key= lambda x:x[0])[0]
+            ymin = min(blob, key= lambda x:x[1])[1]
+            ymax = max(blob, key= lambda x:x[1])[1]
+            yield data[xmin:xmax+1,ymin:ymax+1], xmin,xmax,ymin,ymax
+
+
+
 def unravel_nans(arr,mask,shape):
     """
     Assume that 
@@ -47,32 +168,27 @@ def two_d_gaussian(x, y, amp, xo, yo, major, minor, pa):
 def gaussian(x, amp, cen, wid):
     return amp * np.exp(-(x-cen)**2 / wid)
 
-
 def ntwodgaussian(inpars):
     """
-    Return an array of values represented by multiple Gaussians as parameterized
-    by params = [amp,x0,y0,major,minor,pa]{n}
-    x0,y0,major,minor are in pixels
-    major/minor are interpreted as being sigmas not FWHMs
-    pa is in degrees
     """
-    if not len(inpars)%6 ==0:
-        logging.error("inpars requires a multiple of 6 parameters")
-        logging.error("only {0} parameters supplied".format(len(inpars)))
-        sys.exit()
-    #pars=np.array(inpars).reshape(len(inpars)/6,6)
-    amp,xo,yo,major,minor,pa = np.array(inpars).reshape(6,len(inpars)/6)
-    #transform pa->-pa so that our angles are CW instead of CCW
-    st,ct,s2t=zip(*[ (math.sin(np.radians(-p))**2,math.cos(np.radians(-p))**2,math.sin(2*np.radians(-p))) for p in pa])
-    a, bb, c = zip(*[ ((ct[i]/major[i]**2 + st[i]/minor[i]**2)/2,
-                       s2t[i]/4 *(1/minor[i]**2-1/major[i]**2),
-                       (st[i]/major[i]**2 + ct[i]/minor[i]**2)/2) for i in xrange(len(amp))])
+
+    amp = inpars[::6]
+    xo = inpars[1::6]
+    yo = inpars[2::6]
+    major = inpars[3::6]
+    minor = inpars[4::6]
+    pa = inpars[5::6]
+
+    #st,ct,s2t=zip(*[ (math.sin(p)**2,math.cos(p)**2,math.sin(2*p)) for p in pa])
+    #a, bb, c = zip(*[ ((ct[i]/major[i]**2 + st[i]/minor[i]**2)/2,
+    #                   s2t[i]/4 *(1/minor[i]**2-1/major[i]**2),
+    #                   (st[i]/major[i]**2 + ct[i]/minor[i]**2)/2) for i in xrange(len(amp))])
 
     def rfunc(x,y):
         ans=0
         #list comprehension just breaks here, something to do with scope i think
         for i in range(len(amp)):
-            ans+= amp[i]*np.exp(-1*(a[i]*(x-xo[i])**2 + 2*bb[i]*(x-xo[i])*(y-yo[i]) + c[i]*(y-yo[i])**2) )
+            ans+= two_d_gaussian(y,x,amp[i], xo[i], yo[i], major[i], minor[i], pa[i])
         return ans
     return rfunc
 
@@ -102,7 +218,6 @@ def multi_gauss(data,parinfo):
     mp=mpfit(erfunc,parinfo=parinfo,quiet=True)
 
     return mp,parinfo
-
 
 def test1d():
     x = np.linspace(-5,5,100)
@@ -257,10 +372,9 @@ def test2d2():
 
     pyplot.show()
 
-# @profile
-def test_lm(data):
+#@profile
+def test_lm(data,x,y,init_pars):
 
-    x,y = np.meshgrid(range(data.shape[0]),range(data.shape[1]))
     # convert 2d data into 1d lists, masking out nans in the process
     data, mask, shape = ravel_nans(data)
     x = np.ravel(x[mask])
@@ -268,115 +382,136 @@ def test_lm(data):
 
     # setup the fitting
     g1 = lmfit.Model(two_d_gaussian,independent_vars=['x','y'],prefix="c1_") 
-    g1.set_param_hint('amp',value=1)
-    g1.set_param_hint('xo',value=1.2) 
-    g1.set_param_hint('yo',value=1)
-    g1.set_param_hint('major', value=2, min=1, max=4)
-    g1.set_param_hint('minor', value=1, min=0.5, max=3)
-    g1.set_param_hint('pa', value = 0 , min=-math.pi, max=math.pi)
+    g1.set_param_hint('amp',value=init_pars[0])
+    g1.set_param_hint('xo',value=init_pars[1]) 
+    g1.set_param_hint('yo',value=init_pars[2])
+    g1.set_param_hint('major', value=init_pars[3])
+    g1.set_param_hint('minor', value=init_pars[4])
+    g1.set_param_hint('pa', value = init_pars[5] , min=-math.pi, max=math.pi)
 
     g2 = lmfit.Model(two_d_gaussian,independent_vars=['x','y'],prefix="c2_") 
-    g2.set_param_hint('amp',value=1)
-    g2.set_param_hint('xo',value=3) 
-    g2.set_param_hint('yo',value=3.1)
-    g2.set_param_hint('major', value=2, min=0.5, max=2)
-    g2.set_param_hint('minor', value=1, min=0.5, max=2)
-    g2.set_param_hint('pa', value = 0 , min=-math.pi, max=math.pi)
+    g2.set_param_hint('amp',value=init_pars[6])
+    g2.set_param_hint('xo',value=init_pars[7]) 
+    g2.set_param_hint('yo',value=init_pars[8])
+    g2.set_param_hint('major', value=init_pars[9])
+    g2.set_param_hint('minor', value=init_pars[10])
+    g2.set_param_hint('pa', value =init_pars[11] , min=-math.pi, max=math.pi)
 
     #do the fit
     gmod = reduce(lambda x,y: x+y,[g1+g2])
     params = gmod.make_params()
     result = gmod.fit(data,x=x,y=y,params=params)
-    return unravel_nans(result.best_fit,mask,shape),result.values
+    keylist = [a+b for a in ['c1_','c2_'] for b in ['amp','xo','yo','major','minor','pa']]
+    return unravel_nans(result.best_fit,mask,shape), [result.values[k] for k in keylist]
 
-# @profile
-def test_mpfit(data):
+#@profile
+def test_mpfit(data,init_pars):
     i=1
     parinfo=[]
-    parinfo.append( {'value':1,
+    parinfo.append( {'value':init_pars[0],
        'fixed':False,
        'parname':'{0}:amp'.format(i),
-       'limits':[0,2],
-       'limited':[False,False]} )
-    parinfo.append( {'value':1.2,
+       'limits':[init_pars[0]*0.8, init_pars[0]*1.2],
+       'limited':[True,True]} )
+
+    parinfo.append( {'value':init_pars[1],
        'fixed':False,
        'parname':'{0}:xo'.format(i),
-       'limits':[0,0],
-       'limited':[False,False]} )
-    parinfo.append( {'value':1,
+       'limits':[init_pars[1]*0.8,init_pars[1]*1.2],
+       'limited':[True,True]} )
+
+    parinfo.append( {'value':init_pars[2],
        'fixed':False,
        'parname':'{0}:yo'.format(i),
-       'limits':[0,0],
-       'limited':[False,False]} )
-    parinfo.append( {'value':2,
+       'limits':[init_pars[2]*0.8,init_pars[2]*1.2],
+       'limited':[True,True]} )
+
+    parinfo.append( {'value':init_pars[3],
        'fixed': False,
        'parname':'{0}:major'.format(i),
-       'limits':[1,4],
+       'limits':[init_pars[3]*0.8,init_pars[3]*1.2],
        'limited':[True,True]} )
-    parinfo.append( {'value':1,
+
+    parinfo.append( {'value':init_pars[4],
        'fixed': False,
        'parname':'{0}:minor'.format(i),
-       'limits':[0.5,3],
+       'limits':[init_pars[4]*0.8,init_pars[4]*1.2],
        'limited':[True,True]} )
-    parinfo.append( {'value':0,
+
+    parinfo.append( {'value':init_pars[5],
        'fixed': False,
        'parname':'{0}:pa'.format(i),
        'limits':[-180,180],
        'limited':[False,False]} )
+
     i=2
-    parinfo.append( {'value':1,
+    parinfo.append( {'value':init_pars[6],
        'fixed':False,
        'parname':'{0}:amp'.format(i),
-       'limits':[0,2],
-       'limited':[False,False]} )
-    parinfo.append( {'value':3,
+       'limits':[init_pars[6]*0.8,init_pars[6]*1.2],
+       'limited':[True,True]} )
+
+    parinfo.append( {'value':init_pars[7],
        'fixed':False,
        'parname':'{0}:xo'.format(i),
-       'limits':[0,0],
-       'limited':[False,False]} )
-    parinfo.append( {'value':3.1,
+       'limits':[init_pars[7]*0.8,init_pars[7]*1.2],
+       'limited':[True,True]} )
+
+    parinfo.append( {'value':init_pars[8],
        'fixed':False,
        'parname':'{0}:yo'.format(i),
-       'limits':[0,0],
-       'limited':[False,False]} )
-    parinfo.append( {'value':2,
+       'limits':[init_pars[8]*0.8,init_pars[8]*1.2],
+       'limited':[True,True]} )
+
+    parinfo.append( {'value':init_pars[9],
        'fixed': False,
        'parname':'{0}:major'.format(i),
-       'limits':[0.5,2],
+       'limits':[init_pars[9]*0.8,init_pars[9]*1.2],
        'limited':[True,True]} )
-    parinfo.append( {'value':1,
+
+    parinfo.append( {'value':init_pars[10],
        'fixed': False,
        'parname':'{0}:minor'.format(i),
-       'limits':[0.5,2],
+       'limits':[init_pars[10]*0.8,init_pars[10]*1.2],
        'limited':[True,True]} )
-    parinfo.append( {'value':0,
+
+    parinfo.append( {'value':init_pars[11],
        'fixed': False,
        'parname':'{0}:pa'.format(i),
        'limits':[-180,180],
        'limited':[False,False]} )
+
+    #print parinfo
     mp, parinfo = multi_gauss(data,parinfo)
 
-    #return the data
-    #print parinfo
-    inpars = [a['value'] for a in parinfo]
-    #print inpars
-    ret = ntwodgaussian(inpars)(*np.indices(data.shape))
-    print ret.shape
-    return ret,inpars
+    ret = ntwodgaussian(mp.params)(*np.indices(data.shape))
+    return ret, mp.params
 
 
 def compare():
-    dxy = 20
-    dim = np.linspace(0,5,dxy)
+    dxy = 40
+    dim = range(dxy)
     x,y = np.meshgrid(dim,dim)
-    ztrue = two_d_gaussian(x,y, 1, 1, 1, 3, 1, 0) + two_d_gaussian(x,y, 1,3,3,1,1,0)
-    z = ztrue + 0.4*(0.5-np.random.random((dxy,dxy)))
-    z[:10,13:14]=np.nan
 
-    lm_data, lm_pars = test_lm(z)
-    mp_data, mp_pars = test_mpfit(z)
+    t_pars = [ 1,20,20,6,3,0,   1,10,30,3,3,0]
+    init_pars = t_pars *(1+0.2*(0.5-np.random.random(len(t_pars))))
 
-    print lm_pars, mp_pars
+    ztrue = two_d_gaussian(x,y, *t_pars[:6]) + two_d_gaussian(x,y, *t_pars[6:])
+    z = ztrue + 0.1*(0.5-np.random.random((dxy,dxy)))
+    #z[:10,13:14]=np.nan
+
+    #z=ztrue
+
+    lm_data, lm_pars = test_lm(z,x,y,init_pars)
+    mp_data, mp_pars = test_mpfit(z,init_pars)
+
+    
+    s_pars = ['amp','xo','yo','maj','min','pa']*2
+
+    print "results:"
+    print "     input  init    lm    mp"
+    for l,m,t,s,i in zip(lm_pars,mp_pars,t_pars,s_pars,init_pars):
+        print "{3:4s} {2:5.2f} {4:5.2f} {0:5.2f} {1:5.2f}".format(l,m,t,s,i)
 
     from matplotlib import pyplot
     fig=pyplot.figure()
@@ -399,7 +534,69 @@ def compare():
 
     pyplot.show()
 
+def prof_comp():
+    dxy = 25
+    dim = range(dxy)
+    x,y = np.meshgrid(dim,dim)
+
+    t_pars = [ 1,10,10,3,1,0,   1,13,17,1,1,0]
+    init_pars = t_pars +0.01*np.random.random(len(t_pars))
+
+    ztrue = two_d_gaussian(x,y, *t_pars[:6]) + two_d_gaussian(x,y, *t_pars[6:])
+    z = ztrue + 0.1*(0.5-np.random.random((dxy,dxy)))
+    z[:10,13:14]=np.nan
+
+    #z=ztrue
+    for i in xrange(10):
+        init_pars = t_pars+0.01*np.random.random(len(t_pars))
+        lm_data, lm_pars = test_lm(z,x,y,init_pars)
+        mp_data, mp_pars = test_mpfit(z,init_pars)
+
+def scipy_segment(data, rmsimg, innerclip,outerclip=None,domask=False):
+    """
+    """
+    if outerclip is None:
+        outerclip = innerclip
+
+    snr = data/rmsimg
+    a =  snr > outerclip
+    l, n = label(a)
+    f = find_objects(l)
+
+    for i in range(n):
+        xmin,xmax = f[i][0].start, f[i][0].stop
+        ymin,ymax = f[i][1].start, f[i][1].stop
+        if np.any(snr[xmin:xmax,ymin:ymax]>innerclip):
+            yield data[xmin:xmax,ymin:ymax], xmin, xmax, ymin, ymax
+
+def test_segment():
+    """
+    """
+    a = np.array([[0,1,1,0,0,0],[0,1,1,0,1,0],[0,0,0,1,1,1],[0,0,0,0,1,0]])
+    b = np.ones(a.shape)*0.1
+    innerclip=5
+    print a
+    for i,j in zip(scipy_segment(a,b,innerclip),gen_flood_wrap(a,b,innerclip)):
+        print i
+        print j
+        print 
+
+@profile
+def prof_segment():
+    """
+    """
+    for i in range(10):
+        a = np.random.random((100,100))
+        b = np.ones(a.shape)*0.2
+        innerclip = 2
+        outerclip = 1
+        t1 = [ j for j in scipy_segment(a,b,innerclip,outerclip)]
+        t2 = [ j for j in gen_flood_wrap(a,b,innerclip,outerclip)]
+        print len(t1), len(t2)
 
 if __name__ == '__main__':
     # test2d2()
-    compare()
+    # compare()
+    # prof_comp()
+    # test_segment()
+    prof_segment()
