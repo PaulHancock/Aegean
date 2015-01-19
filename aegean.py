7aeeae2942a3c5f50a56d4bc858d7e11c68a0067
PaulHancock <paul.hancock@curtin.edu.au>
2015-01-19 16:32:35 +0800

    get_pixbeam now gives expected results and is used consistently v+

--- aegean.py
+++ aegean.py
@@ -66,7 +66,7 @@ import AegeanTools.pprocess as pprocess
 import multiprocessing
 
 # Aegean version [Updated via script]
-version = 'v1.9rc1-75-gac488ac'
+version = 'v1.9rc1-78-gc4461b1'
 
 header = """#Aegean version {0}
 # on dataset: {1}"""
