f810e57ffe1db72dd16101ea79d5078ef02d78d5
PaulHancock <paul.hancock@curtin.edu.au>
2015-02-02 15:49:47 +0800

    Fixed a bug that stopped negative sources from being found. v+

--- aegean.py
+++ aegean.py
@@ -66,7 +66,7 @@ import AegeanTools.pprocess as pprocess
 import multiprocessing
 
 # Aegean version [Updated via script]
-version = 'v1.9rc1-80-gf3cc16f'
+version = 'v1.9rc1-84-g63325f4'
 
 header = """#Aegean version {0}
 # on dataset: {1}"""
