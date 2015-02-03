35d39003d642b17ab7bf0dba1add9f86b056841a
PaulHancock <paul.hancock@curtin.edu.au>
2015-02-03 18:22:51 +0800

    pep things

--- AegeanTools/fits_image.py
+++ AegeanTools/fits_image.py
@@ -1,15 +1,17 @@
-'''
+"""
 Created on 18/07/2011
 
 @author: jay
-'''
+"""
 
 import astropy.io.fits as pyfits
 import numpy
 import astropy.wcs as pywcs
 import scipy.stats
-import logging,sys
+import logging
+import sys
 from fits_interp import expand
+
 
 def load_safe(filename):
     """
@@ -19,12 +21,13 @@ def load_safe(filename):
     """
     try:
         hdus = pyfits.open(filename)
-    except IOError,e:
+    except IOError, e:
         if "END" in e.message:
             logging.warn(e.message)
         logging.warn("trying to ignore this, but you should really fix it")
-        hdus = pyfits.open(filename,ignore_missing_end=True)
+        hdus = pyfits.open(filename, ignore_missing_end=True)
     return hdus
+
 
 class FitsImage():
     def __init__(self, filename=None, hdu_index=0, beam=None):
@@ -34,11 +37,11 @@ class FitsImage():
         hdu = a pyfits hdu. if provided the object is constructed from this instead of
               opening the file (filename is ignored)  
         """
-        if isinstance(filename,pyfits.HDUList):
+        if isinstance(filename, pyfits.HDUList):
             logging.debug("accepting already loaded file {0}".format(filename.filename()))
             # check to see if this image has been compressed
             # and expand if neccessary
-            if 'BN_CFAC' in filename.header:
+            if 'BN_CFAC' in filename[hdu_index].header:
                 logging.debug("Expanding file")
                 self.hdu = expand(filename)[hdu_index]
             else:
@@ -53,50 +56,51 @@ class FitsImage():
                 self.hdu = expand(hdulist)[hdu_index]
             del hdulist
         
-        self._header=self.hdu.header
-        #need to read these headers before we 'touch' the data or they dissappear
+        self._header = self.hdu.header
+        # need to read these headers before we 'touch' the data or they dissappear
         if "BZERO" in self._header:
-            self.bzero= self._header["BZERO"]
+            self.bzero = self._header["BZERO"]
         else:
-            self.bzero=0
+            self.bzero = 0
         if "BSCALE" in self._header:
-            self.bscale=self._header["BSCALE"]
+            self.bscale = self._header["BSCALE"]
         else:
-            self.bscale=1
+            self.bscale = 1
             
         self.filename = filename
-        #fix possible problems with miriad generated fits files % HT John Morgan.
+        # fix possible problems with miriad generated fits files % HT John Morgan.
         try:
             self.wcs = pywcs.WCS(self._header, naxis=2)
         except:
-            self.wcs = pywcs.WCS(str(self._header),naxis=2)
+            self.wcs = pywcs.WCS(str(self._header), naxis=2)
             
         self.x = self._header['NAXIS1']
         self.y = self._header['NAXIS2']
-        #this is correct at the center of the image for all images, and everywhere for conformal projections
-        if all( [ a in self._header for a in ["CDELT1","CDELT2"]]):
+        # this is correct at the center of the image for all images, and everywhere for conformal projections
+        if all([a in self._header for a in ["CDELT1", "CDELT2"]]):
             self.pixarea = abs(self._header["CDELT1"]*self._header["CDELT2"])
             self.pixscale = (self._header["CDELT1"], self._header["CDELT2"])
-        elif all( [a in self._header for a in ["CD1_1","CD1_2","CD2_1","CD2_2"]]):
-            self.pixarea = abs( self._header["CD1_1"]*self._header["CD2_2"] - self._header["CD1_2"]*self._header["CD2_1"])
+        elif all([a in self._header for a in ["CD1_1", "CD1_2", "CD2_1", "CD2_2"]]):
+            self.pixarea = abs(self._header["CD1_1"]*self._header["CD2_2"]
+                               - self._header["CD1_2"]*self._header["CD2_1"])
             self.pixscale = (self._header["CD1_1"], self._header["CD2_2"])
-            if not (self._header["CD1_2"] ==0 and self._header["CD2_1"]==0):
+            if not (self._header["CD1_2"] == 0 and self._header["CD2_1"] == 0):
                 logging.warn("Pixels don't appear to be square -> pixscale is wrong")
-        elif all([a in self._header for a in ["CD1_1","CD2_2"]]):
+        elif all([a in self._header for a in ["CD1_1", "CD2_2"]]):
             self.pixarea = abs(self._header["CD1_1"]*self._header["CD2_2"])
             self.pixscale = (self._header["CD1_1"], self._header["CD2_2"])
         else:
             logging.warn("cannot determine pixel area, using zero EVEN THOUGH THIS IS WRONG!")
             self.pixarea = 0
-            self.pixscale = (0,0)
+            self.pixscale = (0, 0)
         
         if beam is None:
-            #if the bpa isn't specified add it as zero
+            # if the bpa isn't specified add it as zero
             if "BPA" not in self._header:
                 logging.info("BPA not present in fits header, using 0")
-                bpa=0
+                bpa = 0
             else:
-                bpa=self._header["BPA"]
+                bpa = self._header["BPA"]
                 
             if "BMAJ" not in self._header:
                 logging.error("BMAJ not present in fits header.")
@@ -111,21 +115,20 @@ class FitsImage():
                 sys.exit(0)
             else:
                 bmin = self._header["BMIN"]
-            self.beam=Beam(bmaj, bmin, bpa)
-        else: #use the supplied beam
-            self.beam=beam
+            self.beam = Beam(bmaj, bmin, bpa)
+        else:  # use the supplied beam
+            self.beam = beam
         self._rms = None
         self._pixels = numpy.squeeze(self.hdu.data)
-        #convert +/- inf to nan
+        # convert +/- inf to nan
         self._pixels[numpy.where(numpy.isinf(self._pixels))] = numpy.nan
-        #del self.hdu
-        logging.debug("Using axes {0} and {1}".format(self._header['CTYPE1'],self._header['CTYPE2']))
+        # del self.hdu
+        logging.debug("Using axes {0} and {1}".format(self._header['CTYPE1'], self._header['CTYPE2']))
 
-        
     def get_pixels(self):
         return self._pixels
 
-    def set_pixels(self,pixels):
+    def set_pixels(self, pixels):
         """
         Allow the pixels to be replaced
         Will only work if pixels.shape is the same as self._pixels.shape
@@ -134,16 +137,16 @@ class FitsImage():
         self._pixels = pixels
             
     def get_background_rms(self):
-        '''
+        """
         Return the background RMS (Jy)
         NB - value is calculated on first request then cached for speed
-        '''
-        #TODO: return a proper background RMS ignoring the sources
+        """
+        # TODO: return a proper background RMS ignoring the sources
         # This is an approximate method suggested by PaulH.
         # I have no idea where this magic 1.34896 number comes from...
         if self._rms is None:
             # Get the pixels values without the NaNs
-            data = numpy.extract(self.hdu.data>-9999999, self.hdu.data)
+            data = numpy.extract(self.hdu.data > -9999999, self.hdu.data)
             p25 = scipy.stats.scoreatpercentile(data, 25)
             p75 = scipy.stats.scoreatpercentile(data, 75)
             iqr = p75 - p25
@@ -151,7 +154,9 @@ class FitsImage():
         return self._rms
     
     def pix2sky(self, pixel):
-        '''Get the sky coordinates [ra,dec] (degrees) given pixel [x,y] (float)'''
+        """
+        Get the sky coordinates [ra,dec] (degrees) given pixel [x,y] (float)
+        """
         pixbox = numpy.array([pixel, pixel])
         skybox = self.wcs.wcs_pix2sky(pixbox, 1)
         return [float(skybox[0][0]), float(skybox[0][1])]
@@ -160,7 +165,9 @@ class FitsImage():
         return self._header
 
     def sky2pix(self, skypos):
-        '''Get the pixel coordinates [x,y] (floats) given skypos [ra,dec] (degrees)'''
+        """
+        Get the pixel coordinates [x,y] (floats) given skypos [ra,dec] (degrees)
+        """
         skybox = [skypos, skypos]
         pixbox = self.wcs.wcs_sky2pix(skybox, 1)
         return [float(pixbox[0][0]), float(pixbox[0][1])] 
@@ -171,13 +178,13 @@ class Beam():
     Small class to hold the properties of the beam
     """
     def __init__(self, a, b, pa, pixa=None, pixb=None):
-        assert a>0, "major axis must be >0"
-        assert b>0, "minor axis must be >0"
-        self.a=a
-        self.b=b
-        self.pa=pa
-        self.pixa=pixa
-        self.pixb=pixb
+        assert a > 0, "major axis must be >0"
+        assert b > 0, "minor axis must be >0"
+        self.a = a
+        self.b = b
+        self.pa = pa
+        self.pixa = pixa
+        self.pixb = pixb
     
     def __str__(self):
         return "a={0} b={1} pa={2}".format(self.a, self.b, self.pa)
