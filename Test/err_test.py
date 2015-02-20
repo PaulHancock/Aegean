cf742ae85da56b5f44affb2f6edd9a4756c6f73a
Paul Hancock <paul.hancock@curtin.edu.au>
2015-02-20 23:11:48 +0800

    correct implementation of correlated errors for mpfit

--- Test/err_test.py
+++ Test/err_test.py
@@ -14,6 +14,10 @@ import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 from math import floor
 from AegeanTools.mpfit import mpfit
+from scipy.linalg import sqrtm
+import logging
+
+
 
 def multi_gauss(data, rmsimg, parinfo):
     """
@@ -25,26 +29,28 @@ def multi_gauss(data, rmsimg, parinfo):
     return: mpfit object, parameter info
     """
     shape = data.shape
+    coords = np.where(np.isfinite(data))
     data = np.ravel(data)
     mask = np.where(np.isfinite(data))  #the indices of the *non* NaN values in data
     data = data[mask]
 
-    x, y = np.meshgrid(range(shape[0]),range(shape[1]))
-    z = np.ravel(ae.ntwodgaussian([1, 1, 1, smoothing, smoothing, 0])(x,y))[mask]
+    z = np.ravel(ae.ntwodgaussian([1, 1, 1, smoothing, smoothing, 0])(*coords))
     C = np.vstack((z,)*len(z))
     for i in range(len(z)):
         C[i] = np.roll(C[i],i)
     Ci = np.matrix(C).I
-
+    #Ci = np.matrix(np.diag(np.ones((len(z)))))
+    #Ci = sqrtm(Ci)
     def model(p):
         """Return a map with a number of Gaussians determined by the input parameters."""
-        return np.ravel(ae.ntwodgaussian(p)(x,y))[mask]
+        return np.ravel(ae.ntwodgaussian(p)(*coords))
 
 
     def erfunc(p, fjac=None):
         """The difference between the model and the data"""
         resid = (model(p)-data) * Ci # * np.matrix(model-data).T
-        resid =  resid.tolist()[0]
+        resid =  np.array(resid.tolist()[0])
+        #return [0, model(p) - data ]
         return [0, resid]
 
     mp = mpfit(erfunc, parinfo=parinfo, quiet=True)
@@ -105,8 +111,8 @@ hdulist.writeto('test.fits',clobber=True)
 
 sources = ae.find_sources_in_image(hdulist, max_summits=2, rms=1, cores=1)
 print "found {0}/{1} sources".format(len(sources),f+1)
-ae.save_catalog('test2.vot',sources)
-ae.save_catalog('test.reg',sources)
+ae.save_catalog('test_m.vot',sources)
+ae.save_catalog('test_m.reg',sources)
 
 
 out = open('test.csv','w')
@@ -114,7 +120,7 @@ print >>out, ','.join(['flux', 'ra', 'dec', 'major', 'minor', 'pa'])
 for f, xo, yo in zip(fluxes, rapix, decpix):
     major, minor = smoothing, smoothing
     pa = 0
-    ra, dec = ae.pix2sky([yo,xo])
+    ra, dec = ae.pix2sky([yo+1,xo+1]) # not sure why I need +1 here
     print xo,yo,"->",ra,dec
     print >>out, ','.join(map(str,[f, ra, dec, major*np.sqrt(scale)*ae.cc2fwhm *3600, minor*np.sqrt(scale)*ae.cc2fwhm *3600, pa]))
 out.close
