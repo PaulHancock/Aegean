79c5bc628a4666916b650a747a34598998dc10ef
PaulHancock <paul.hancock@curtin.edu.au>
2015-05-06 18:09:07 +0800

    module to help with clustering analysis

---
+++ AegeanTools/cluster.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+
+"""
+Cluster and crossmatch tools and analysis functions.
+
+DBSCAN clustering
+"""
+
+__author__= "Paul Hancock"
+
+import numpy as np
+import logging
+import sys
+
+from angle_tools import gcd
+import sklearn
+import sklearn.cluster
+from models import SimpleSource
+
+from catalogs import load_table, table_to_source_list
+
+def pairwise_distance(positions):
+    """
+
+    :param positions:
+    :return:
+    """
+    distances = np.empty((len(positions), len(positions)), dtype=np.float32)
+    for i,p1 in enumerate(positions):
+        for j,p2 in enumerate(positions):
+            if j<i: #distances are symmetric so only calculate 1/2 of them
+                continue
+            distances[i, j] = gcd(*np.ravel([p1,p2]))
+            distances[j, i] = distances[i, j]
+    return distances
+
+def group_iter(catalog, eps, min_members=1):
+    """
+    :param catalog:
+    :param eps:
+    :param min_members:
+    :return:
+    """
+
+    if isinstance(catalog,str):
+        table = load_table(catalog)
+        positions = np.array(zip(table['ra'],table['dec']))
+        srccat = np.array(table_to_source_list(table))
+    elif isinstance(catalog,list):
+        try:
+            positions = np.array([(s.ra,s.dec) for s in catalog])
+            srccat = np.array(catalog)
+        except AttributeError:
+            logging.error("catalog is as list of something that has not ra/dec attributes")
+            sys.exit(1)
+    else:
+        logging.error("I don't know what catalog is")
+        sys.exit(1)
+
+    X = pairwise_distance(positions)
+    samples, labels = sklearn.cluster.dbscan(X,eps=eps, min_samples=min_members, metric='precomputed')
+    # remove repeats and the noise flag of -1
+    unique_labels = set(labels).difference(set([-1]))
+    for l in unique_labels:
+        class_member_mask = (labels == l)
+        yield srccat[class_member_mask]
