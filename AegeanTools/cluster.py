1fa938d0374c6f0fc0eba42e6d61fd4541b18c3c
PaulHancock <paul.hancock@curtin.edu.au>
2015-05-08 15:49:45 +0800

    probabilities replaced with normalised distances for eps = sqrt(2) sources with FWHM that 'touch' are grouped together

--- AegeanTools/cluster.py
+++ AegeanTools/cluster.py
@@ -12,7 +12,7 @@ import numpy as np
 import sys
 import math
 
-from angle_tools import gcd
+from angle_tools import gcd, bear
 import sklearn
 import sklearn.cluster
 
@@ -25,6 +25,48 @@ log = logging.getLogger('Aegean')
 
 cc2fwhm = (2 * math.sqrt(2 * math.log(2)))
 fwhm2cc = 1/cc2fwhm
+
+def norm_dist(src1,src2):
+    """
+    Calculate the normalised distance between two sources.
+    Sources are elliptical gaussians.
+    :param src1:
+    :param src2:
+    :return:
+    """
+    if src1 == src2:
+        return 0
+    dist = gcd(src1.ra, src1.dec, src2.ra, src2.dec) # degrees
+
+    # the angle between the ellipse centers
+    phi = bear(src1.ra, src1.dec, src2.ra, src2.dec) # Degrees
+    # Calculate the radius of each ellpise along a line that joins their centers.
+    r1 = src1.a*src1.b / np.hypot(src1.a * np.sin(np.radians(phi - src1.pa)),
+                                  src1.b * np.cos(np.radians(phi - src1.pa)))
+    r2 = src2.a*src2.b / np.hypot(src2.a * np.sin(np.radians(180 + phi - src2.pa)),
+                                  src2.b * np.cos(np.radians(180 + phi - src2.pa)))
+    R = dist / (np.hypot(r1,r2) / 3600)
+    return R
+
+
+def pairwise_ellpitical(sources):
+    """
+    Calculate the probability of an association between each pair of sources.
+    0<= probability <=1
+    Form this into a matrix.
+    :param sources: A list of sources
+    :return: a matrix of probabilities.
+    """
+    probabilities = np.empty((len(sources), len(sources)), dtype=np.float32)
+    for i,src1 in enumerate(sources):
+        for j,src2 in enumerate(sources):
+            if j<i:
+                continue
+            # TODO: write an early-out for very distant sources
+            probabilities[i, j] = norm_dist(src1, src2)
+            probabilities[j, i] = probabilities[i, j]
+    return probabilities
+
 
 def pairwise_distance(positions):
     """
@@ -43,44 +85,7 @@ def pairwise_distance(positions):
     return distances
 
 
-def fa2cd(ab):
-    """
-    Convert fwhm in arcseconds to sigmas in degrees
-    :param ab:
-    :return:
-    """
-    return ab/3600.*fwhm2cc
-
-def prob_elliptical(src1,src2):
-    if src1 == src2:
-        return 0
-    #print src1.ra,src1.dec,fa2cd(src1.a),fa2cd(src1.b),-1*np.radians(src1.pa)
-    #print src2.ra,src2.dec,fa2cd(src2.a),fa2cd(src2.b),-1*np.radians(src2.pa)
-    p1 = elliptical_gaussian(src2.ra,src2.dec,1.,src1.ra,src1.dec,fa2cd(src1.a),fa2cd(src1.b),-1*np.radians(src1.pa))
-    p2 = elliptical_gaussian(src1.ra,src1.dec,1.,src2.ra,src2.dec,fa2cd(src2.a),fa2cd(src2.b),-1*np.radians(src2.pa))
-    print p1, p2
-    return 1- np.sqrt(p1*p2)
-
-
-def pairwise_probability(sources):
-    """
-    Calculate the probability of an association between each pair of sources.
-    0<= probability <=1
-    Form this into a matrix.
-    :param sources: A list of sources
-    :return: a matrix of probabilities.
-    """
-    probabilities = np.empty((len(sources), len(sources)), dtype=np.float32)
-    for i,src1 in enumerate(sources):
-        for j,src2 in enumerate(sources):
-            if j<i:
-                continue
-            probabilities[i, j] = prob_elliptical(src1, src2)
-            probabilities[j, i] = probabilities[i, j]
-    return probabilities
-
-
-def group_iter(catalog, eps, min_members=1, prob=False):
+def group_iter(catalog, eps, min_members=1, norm=False):
     """
     :param catalog: List of sources, or filename of a catalog
     :param eps: Clustering radius in *degrees*
@@ -102,16 +107,26 @@ def group_iter(catalog, eps, min_members=1, prob=False):
     else:
         logging.error("I don't know what catalog is")
         sys.exit(1)
-
+    log.info("Regrouping islands within catalog")
     log.debug("Calculating distances")
-    if prob:
-        X = pairwise_probability(srccat)
+    if norm:
+        X = pairwise_ellpitical(srccat)
     else:
         X = pairwise_distance(positions)
     log.debug("Clustering")
-    samples, labels = sklearn.cluster.dbscan(X,eps=eps, min_samples=min_members, metric='precomputed')
-    # remove repeats and the noise flag of -1
-    unique_labels = set(labels).difference(set([-1]))
+    off_diag = X[np.where(X>0)]
+    if min(off_diag) > eps:
+        unique_labels = np.arange(len(srccat))
+        labels = unique_labels
+        log.info("None of the sources are clustered.")
+    elif max(off_diag) < eps:
+        unique_labels = [1]
+        labels = unique_labels
+        log.warn("All of the sources are within the same cluster")
+    else:
+        samples, labels = sklearn.cluster.dbscan(X,eps=eps, min_samples=min_members, metric='precomputed')
+        # remove repeats and the noise flag of -1
+        unique_labels = set(labels).difference(set([-1]))
     # Return groups of sources
     for l in unique_labels:
         class_member_mask = (labels == l)
@@ -119,9 +134,17 @@ def group_iter(catalog, eps, min_members=1, prob=False):
 
 
 if __name__ == "__main__":
-    catalog = 'Test/Catalogs/1904_comp.vot'
+    logging.basicConfig()
+    log = logging.getLogger('Aegean')
+    catalog = '1904_comp.vot'
     table = load_table(catalog)
     positions = np.array(zip(table['ra'],table['dec']))
     srccat = list(table_to_source_list(table))
-    for g in group_iter(srccat,0.999,prob=True):
+    X = pairwise_ellpitical(srccat)
+#    print positions[:10]
+    #print X[0]
+    #print gcd(srccat[0].ra,srccat[0].dec, srccat[1].ra, srccat[1].dec)
+    #print norm_dist(srccat[0],srccat[1])
+
+    for g in group_iter(srccat, 2. ,norm=True):
         print len(g),[(a.island,a.source) for a in g]
