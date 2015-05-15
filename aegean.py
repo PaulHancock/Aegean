d49a5ba86d009ffc183b383fd1a92462056730cb
PaulHancock <paul.hancock@curtin.edu.au>
2015-05-15 14:49:19 +0800

    delete commented code

--- aegean.py
+++ aegean.py
@@ -683,6 +683,7 @@ def load_globals(filename, hdu_index=0, bkgin=None, rmsin=None, beam=None, verb=
     global_data.rmsimg = np.zeros(global_data.data_pix.shape, dtype=global_data.dtype)
     global_data.pixarea = img.pixarea
     global_data.telescope_lat = lat
+    global_data.wcshelper.lat = lat
     global_data.dcurve = None
     if do_curve:
         #calculate curvature but store it as -1,0,+1
@@ -1074,136 +1075,16 @@ def scope2lat(telescope):
         log.warn("integrated fluxes may be incorrect")
         return None
 
-
+pass # The following have been moved into AegeanTools.wcs_helpers
+# and can now be accessed via global_data.wcshelper.<func>
 # def pix2sky(pixel):
-#     """
-#     Take pixel=(x,y) coords
-#     convert to pos=(ra,dec) coords
-#     """
-#     x, y = pixel
-#     #wcs and pyfits have oposite ideas of x/y
-#     return global_data.wcs.wcs_pix2world([[y, x]], 1)[0]
-#
-#
 # def sky2pix(pos):
-#     """
-#     Take pos = (ra,dec) coords
-#     convert to pixel = (x,y) coords
-#     """
-#     pixel = global_data.wcs.wcs_world2pix([pos], 1)
-#     #wcs and pyfits have oposite ideas of x/y
-#     return [pixel[0][1], pixel[0][0]]
-#
-#
 # def sky2pix_vec(pos, r, pa):
-#     """Convert a vector from sky to pixel corrds
-#     vector is calculated at an origin pos=(ra,dec)
-#     and has a magnitude (r) [in degrees]
-#     and an angle (pa) [in degrees]
-#     input:
-#         pos - (ra,dec) of vector origin
-#         r - magnitude in degrees
-#         pa - angle in degrees
-#     return:
-#     x,y - corresponding to position ra,dec
-#     r,theta - magnitude (pixels) and angle (degrees) of the original vector
-#     """
-#     ra, dec = pos
-#     x, y = sky2pix(pos)
-#     a = translate(ra, dec, r, pa)
-#     #[ra +r*np.sin(np.radians(pa))*np.cos(np.radians(dec)),
-#     #     dec+r*np.cos(np.radians(pa))]
-#     locations = sky2pix(a)
-#     x_off, y_off = locations
-#     a = np.sqrt((x - x_off) ** 2 + (y - y_off) ** 2)
-#     theta = np.degrees(np.arctan2((y_off - y), (x_off - x)))
-#     return x, y, a, theta
-#
-#
 # def pix2sky_vec(pixel, r, theta):
-#     """
-#     Convert a vector from pixel to sky coords
-#     vector is calculated at an origin pixel=(x,y)
-#     and has a magnitude (r) [in pixels]
-#     and an angle (theta) [in degrees]
-#     input:
-#         pixel - (x,y) of origin
-#         r - magnitude in pixels
-#         theta - in degrees
-#     return:
-#     ra,dec - corresponding to pixels x,y
-#     r,pa - magnitude and angle (degrees) of the original vector, as measured on the sky
-#     """
-#     ra1, dec1 = pix2sky(pixel)
-#     x, y = pixel
-#     a = [x + r * np.cos(np.radians(theta)),
-#          y + r * np.sin(np.radians(theta))]
-#     locations = pix2sky(a)
-#     ra2, dec2 = locations
-#     a = gcd(ra1, dec1, ra2, dec2)
-#     pa = bear(ra1, dec1, ra2, dec2)
-#     return ra1, dec1, a, pa
-#
-#
 # def get_pixbeam():
-#     """
-#     Use global_data to get beam (sky scale), and img.pixscale.
-#     Calculate a beam in pixel scale, pa is always zero
-#     :return: A beam in pixel scale
-#     """
-#     global global_data
-#     beam = global_data.beam
-#     pixscale = global_data.img.pixscale
-#     # TODO: update this to incorporate elevation scaling when needed
-#     major = beam.a/(pixscale[0]*math.sin(math.radians(beam.pa)) +pixscale[1]*math.cos(math.radians(beam.pa)) )
-#     minor = beam.b/(pixscale[1]*math.sin(math.radians(beam.pa)) +pixscale[0]*math.cos(math.radians(beam.pa)) )
-#     # TODO: calculate the pa of the pixbeam
-#     return Beam(abs(major),abs(minor),0)
-#
-#
 # def get_beamarea_deg2(ra,dec):
-#     """
-#
-#     :param ra:
-#     :param dec:
-#     :return:
-#     """
-#     beam = global_data.beam
-#     barea = abs(beam.a * beam.b * np.pi) # in deg**2 at reference coords
-#     if global_data.telescope_lat is not None:
-#         barea /= np.cos(np.radians(dec-global_data.telescope_lat))
-#     return barea
-#
-#
 # def get_beamarea_pix(ra,dec):
-#     """
-#     Calculate the area of the beam at a given location
-#     scale area based on elevation if the telescope latitude is known.
-#     :param ra:
-#     :param dec:
-#     :return:
-#     """
-#     pixscale = global_data.img.pixscale
-#     parea = abs(pixscale[0] * pixscale[1]) # in deg**2 at reference coords
-#     barea = get_beamarea_deg2(ra,dec)
-#     return barea/parea
-#
-#
-#
 # def sky_sep(pix1, pix2):
-#     """
-#     calculate the sky separation between two pixels
-#     Input:
-#         pix1 = [x1,y1]
-#         pix2 = [x2,y2]
-#     Returns:
-#         sep = separation in degrees
-#     """
-#     pos1 = pix2sky(pix1)
-#     pos2 = pix2sky(pix2)
-#     sep = gcd(pos1[0], pos1[1], pos2[0], pos2[1])
-#     return sep
-#
 
 ######################################### THE MAIN DRIVING FUNCTIONS ###############
 
