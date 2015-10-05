454020894232e782ce246dda87e1d92b3a414c32
PaulHancock <paul.hancock@curtin.edu.au>
2015-10-05 17:33:10 +0800

    replaced running percentile with sigma clipping and was rewarded with a runtime reduction of about 90% The old running percentiles method is still available with the --RP option, which is OFF by default.

--- BANE.py
+++ BANE.py
@@ -11,9 +11,10 @@ import copy
 from tempfile import NamedTemporaryFile
 import time
 
-#image manipulation 
 from scipy.interpolate import LinearNDInterpolator, interp2d
+from scipy.stats import sigmaclip
 from astropy.io import fits
+
 
 #Aegean tools
 from AegeanTools.running_percentile import RunningPercentiles as RP
@@ -25,6 +26,141 @@ import multiprocessing
 __author__ = 'Paul Hancock'
 __version__ = 'v1.2'
 __date__ = '2015-06-24'
+
+
+def sigma_filter(filename, region, step_size, box_size, shape, ibkg=None, irms=None):
+    """
+
+    :param filename:
+    :param region:
+    :param step_size:
+    :param box_size:
+    :param shape:
+    :param ibkg:
+    :param irms:
+    :return:
+    """
+
+    # Caveat emptor: The code that follows is very difficult to read.
+    # xmax is not x_max, and x,y actually should be y,x
+    # TODO: fix the code below so that the above comment can be removed
+
+    ymin, ymax, xmin, xmax = region
+
+    logging.debug('{0}x{1},{2}x{3} starting at {4}'.format(xmin,xmax,ymin,ymax,strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+
+    cmin = max(0, ymin - box_size[1]/2)
+    cmax = min(shape[1], ymax + box_size[1]/2)
+    rmin = max(0, xmin - box_size[0]/2)
+    rmax = min(shape[0], xmax + box_size[0]/2)
+
+    # Figure out how many axes are in the datafile
+    NAXIS = fits.getheader(filename)["NAXIS"]
+
+    # It seems that I cannot memmap the same file multiple times without errors
+    with fits.open(filename, memmap=False) as a:
+        if NAXIS ==2:
+            data = a[0].section[rmin:rmax,cmin:cmax]
+        elif NAXIS == 3:
+            data = a[0].section[0,rmin:rmax,cmin:cmax]
+        elif NAXIS ==4:
+            data = a[0].section[0,0,rmin:rmax,cmin:cmax]
+        else:
+            logging.error("Too many NAXIS for me {0}".format(NAXIS))
+            logging.error("fix your file to be more sane")
+            sys.exit(1)
+
+    # x/y min/max should refer to indices into data
+    # this is the region over which we want to operate
+    ymin -= cmin
+    ymax -= cmin
+    xmin -= rmin
+    xmax -= rmin
+
+    def locations(step_size,xmin,xmax,ymin,ymax):
+        """
+        Generator function to iterate over a grid of x,y coords
+        operates only within the given bounds
+        Returns:
+        x,y,previous_x,previous_y
+        """
+
+        xvals = range(xmin,xmax,step_size[0])
+        if xvals[-1]!=xmax:
+            xvals.append(xmax)
+        yvals = range(ymin,ymax,step_size[1])
+        if yvals[-1]!=ymax:
+            yvals.append(ymax)
+        #initial data
+        px,py=xvals[0],yvals[0]
+        i=1
+        for y in yvals:
+            for x in xvals[::i]:
+                yield x,y,px,py
+                px,py=x,y
+            i*=-1 #change x direction
+
+    def box(x,y):
+        """
+        calculate the boundaries of the box centered at x,y
+        with size = box_size
+        """
+        x_min = max(0,x-box_size[0]/2)
+        x_max = min(data.shape[0]-1,x+box_size[0]/2)
+        y_min = max(0,y-box_size[1]/2)
+        y_max = min(data.shape[1]-1,y+box_size[1]/2)
+        return x_min,x_max,y_min,y_max
+
+    bkg_points = []
+    rms_points = []
+    bkg_values = []
+    rms_values = []
+
+    # intialise the rp with our first box worth of data
+    x_min,x_max,y_min,y_max = box(xmin,ymin)
+
+    for x,y,px,py in locations(step_size,xmin,xmax,ymin,ymax):
+        x_min,x_max,y_min,y_max = box(x,y)
+        new = data[x_min:x_max,y_min:y_max]
+        new = np.ravel(new[np.isfinite(new)])
+        new = sigmaclip(new, 3, 3).clipped
+        bkg = np.median(new)
+        rms = np.std(new)
+
+        if bkg is not None:
+            bkg_points.append((x+rmin,y+cmin)) #the coords need to be indices into the larger array
+            bkg_values.append(bkg)
+        if rms is not None:
+            rms_points.append((x+rmin,y+cmin))
+            rms_values.append(rms)
+
+    ymin,ymax,xmin,xmax = region
+    # check if we have been passed some shared memory references
+    # and do the interpolation if we have
+    # otherwise pass back our coords and lists so that interpolation can be done elsewhere
+    if ibkg is not None and irms is not None:
+        gx,gy = np.mgrid[xmin:xmax,ymin:ymax]
+        ifunc = LinearNDInterpolator(rms_points ,rms_values)
+        interpolated_rms = ifunc((gx,gy))
+        with irms.get_lock():
+            for i,row in enumerate(interpolated_rms):
+                start_idx = np.ravel_multi_index((xmin + i,ymin), shape)
+                end_idx = start_idx + len(row)
+                irms[start_idx:end_idx] = row
+
+        ifunc = LinearNDInterpolator(bkg_points ,bkg_values)
+        interpolated_bkg = ifunc((gx,gy))
+        with ibkg.get_lock():
+            for i,row in enumerate(interpolated_bkg):
+                start_idx = np.ravel_multi_index((xmin + i,ymin), shape)
+                end_idx = start_idx + len(row)
+                ibkg[start_idx:end_idx] = row
+        logging.debug('{0}x{1},{2}x{3} finished at {4}'.format(xmin,xmax,ymin,ymax,strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+        return
+    else:
+        logging.debug('{0}x{1},{2}x{3} finished at {4}'.format(xmin,xmax,ymin,ymax,strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+        return xmin, xmax, ymin, ymax, bkg_points, bkg_values, rms_points, rms_values
+
 
 def running_filter(filename, region, step_size, box_size, shape, ibkg=None, irms=None):
     """
@@ -354,7 +490,7 @@ def mask_img(data,mask_data):
 
 
 
-def filter_mc_sharemem(filename, step_size, box_size, cores, shape):
+def filter_mc_sharemem(filename, step_size, box_size, cores, shape, fn=sigma_filter):
     """
     Perform a running filter over multiple cores
     """
@@ -364,7 +500,9 @@ def filter_mc_sharemem(filename, step_size, box_size, cores, shape):
     if cores>1:
         try:
             queue = pprocess.Queue(limit=cores,reuse=1)
-            parfilt = queue.manage(pprocess.MakeReusable(running_filter))
+            parfilt = queue.manage(pprocess.MakeReusable(fn))
+            #parfilt = queue.manage(pprocess.MakeReusable(sigma_filter))
+            #parfilt = queue.manage(pprocess.MakeReusable(running_filter))
             #parfilt = queue.manage(pprocess.MakeReusable(dummy_filter))
         except AttributeError, e:
             if 'poll' in e.message:
@@ -427,7 +565,9 @@ def filter_mc_sharemem(filename, step_size, box_size, cores, shape):
     else:
         #single core we do it all at once
         region = [0,img_x,0,img_y]
-        running_filter(filename,region, step_size, box_size, shape, ibkg, irms)
+        #running_filter(filename,region, step_size, box_size, shape, ibkg, irms)
+        #sigma_filter(filename,region, step_size, box_size, shape, ibkg, irms)
+        fn(filename,region, step_size, box_size, shape, ibkg, irms)
     #and do the interpolation etc...
     logging.debug("reshaping bkg")
     interpolated_bkg = np.reshape(ibkg, shape)
@@ -540,7 +680,7 @@ def filter_mc(filename, step_size, box_size, cores, shape):
     return interpolated_bkg,interpolated_rms
 
 
-def filter_image(im_name, out_base, step_size=None, box_size=None, twopass=False, cores=None, mask=True, compressed=False):
+def filter_image(im_name, out_base, step_size=None, box_size=None, twopass=False, cores=None, mask=True, compressed=False, running=False):
     """
 
     :param im_name:
@@ -551,8 +691,15 @@ def filter_image(im_name, out_base, step_size=None, box_size=None, twopass=False
     :param cores:
     :param mask:
     :param compressed:
+    :param running: use the running percentiles filter.
     :return:
     """
+
+    if running:
+        func = running_filter
+    else:
+        func = sigma_filter
+
     header = fits.getheader(im_name)
     shape = (header['NAXIS2'],header['NAXIS1'])
 
@@ -588,7 +735,7 @@ def filter_image(im_name, out_base, step_size=None, box_size=None, twopass=False
 
     logging.info("using grid_size {0}, box_size {1}".format(step_size,box_size))
     logging.info("on data shape {0}".format(shape))
-    bkg,rms = filter_mc_sharemem(im_name, step_size=step_size, box_size=box_size, cores=cores, shape=shape)
+    bkg, rms = filter_mc_sharemem(im_name, step_size=step_size, box_size=box_size, cores=cores, shape=shape, fn=func)
     logging.info("done")
 
     if twopass:
@@ -601,7 +748,7 @@ def filter_image(im_name, out_base, step_size=None, box_size=None, twopass=False
         temp_name = tempfile.name
         del data, header, tempfile
         logging.info("running second pass to get a better rms")
-        _,rms=filter_mc_sharemem(temp_name,step_size=step_size,box_size=box_size,cores=cores, shape=shape)
+        _, rms = filter_mc_sharemem(temp_name, step_size=step_size, box_size=box_size, cores=cores, shape=shape, fn=func)
         #logging.info("cleaning up temp file {0}".format(tempfile.name))
         os.remove(temp_name)
 
@@ -759,6 +906,8 @@ if __name__=="__main__":
                       help="Don't run if output files already exist. Default is to run+overwrite.")
     parser.add_option('--scipy',dest='usescipy',action='store_true',
                       help='Use scipy generic filter instead of the running percentile filter. (for testing/timing)')
+    parser.add_option('--RP', dest='running', action='store_true', default=False,
+                      help='Use the old/slower running percentiles filter')
     parser.add_option('--debug',dest='debug',action='store_true',help='debug mode, default=False')
     parser.add_option('--compress', dest='compress', action='store_true',default=False,
                       help='Produce a compressed output file.')
@@ -793,5 +942,5 @@ if __name__=="__main__":
     else:
         filter_image(im_name=filename, out_base=options.out_base, step_size=options.step_size,
                      box_size=options.box_size, twopass=options.twopass, cores=options.cores,
-                     mask=options.mask, compressed=options.compress)
+                     mask=options.mask, compressed=options.compress, running=options.running)
 
