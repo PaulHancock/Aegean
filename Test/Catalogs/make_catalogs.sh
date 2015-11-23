9555a271ce6502cfc34c07e5f39a314e339eae94
PaulHancock <paul.hancock@curtin.edu.au>
2015-11-23 17:20:16 +0800

    no longer uses --autoload since it is a little dangerous

--- Test/Catalogs/make_catalogs.sh
+++ Test/Catalogs/make_catalogs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # command line invocations that create the files within this folder
-python aegean.py Test/Images/1904-66_SIN.fits --autoload --table 1904_auto.vot
-python aegean.py Test/Images/Bright.fits --autoload --seedclip 500 --floodclip 100 --table Bright_s500_f100.vot
+python aegean.py Test/Images/1904-66_SIN.fits --background Test/Images/1904-66_SIN_bkg.fits --noise Test/Images/1904-66_SIN_rms.fits --table 1904_auto.vot
+python aegean.py Test/Images/Bright.fits --background Test/Images/1904-66_SIN_bkg.fits --noise Test/Images/1904-66_SIN_rms.fits --seedclip 500 --floodclip 100 --table Bright_s500_f100.vot
