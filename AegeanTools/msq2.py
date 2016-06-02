08f04d5e9703c71aa5ffbbeceeda5d9a9bc5b9b5
PaulHancock <paul.hancock@curtin.edu.au>
2016-06-02 16:27:00 +0800

    Added a new function doMarchAll, which will find the perimeter of all the islands within a dataset. This gives strange results if the islands have 'holes' in them.

--- AegeanTools/msq2.py
+++ AegeanTools/msq2.py
@@ -8,6 +8,7 @@ by Paul Hancock
 May 2014
 """
 
+from copy import copy
 import logging
 import numpy as np
 
@@ -29,6 +30,18 @@ class MarchingSquares():
         self.xsize, self.ysize = data.shape
         self.perimeter = self.doMarch()
         return
+
+    def dir2str(self,d):
+        if d == self.UP:
+            return "U"
+        elif d == self.DOWN:
+            return "D"
+        elif d == self.LEFT:
+            return "L"
+        elif d== self.RIGHT:
+            return "R"
+        else:
+            return "x"
 
     def findStartPoint(self):
         """
@@ -133,26 +146,273 @@ class MarchingSquares():
             #if i>max_tries:
             #	break
             #i+=1
+            print self.dir2str(self.next),
 
+        print 
         return points
 
     def doMarch(self):
         """
         March about and trace the outline of our object
         """
+        x, y = self.findStartPoint()
+        perimeter = self.walkPerimeter(x, y)
+        return perimeter
 
-        perimeter = []
+
+    def _blankWithin(self, perimeter):
+        """
+        Blank all the pixels within the given perimeter.
+        :param perimeter:
+        :return None:
+        """
+        def classify_next(cur,nxt):
+            result = self.NOWHERE
+            if cur[0] - nxt[0] < 0:
+                result = self.RIGHT
+            elif cur[0] - nxt[0] > 0:
+                result = self.LEFT
+            else:
+                if cur[1] - nxt[1] < 0:
+                    result = self.DOWN
+                elif cur[1] - nxt[1] > 0:
+                    result = self.UP
+                else:
+                    result = self.NOWHERE
+            return result
+
+        # Method:
+        # perimeter starts at the top-left most pixel of a region
+        # so we can 'scan right' from each 'DOWN' pixel in the perimeter, blanking as we go
+        # stop when we reach an already blanked pixel
+
+        # include the first pixel as the last pixel so we have a complete circuit.
+        circuit = perimeter+[perimeter[0]]
+        for i, p in enumerate(circuit[:-1]):
+            # figure out which direction we are going to be moving in
+            drn = classify_next(p, circuit[i+1])
+            if drn == self.DOWN:
+                for j in range(p[0], self.xsize):
+                    q = j, p[1]
+                    if self.data[q] == 0:  # stop when we reach an already blank pixel
+                        break
+                    else:
+                        self.data[q] = 0
+            if drn == self.UP:
+                for j in range(self.xsize-1, p[0], -1):from copy import copy
+import logging
+import numpy as np
+
+
+class MarchingSquares():
+    """
+    """
+    NOWHERE = 0b0000
+    UP = 0b0001
+    DOWN = 0b0010
+    LEFT = 0b0100
+    RIGHT = 0b1000
+
+
+    def __init__(self, data):
+        self.prev = self.NOWHERE
+        self.next = self.NOWHERE
+        self.data = np.nan_to_num(data)
+        self.xsize, self.ysize = data.shape
+        self.perimeter = self.doMarch()
+        return
+
+    def dir2str(self,d):
+        if d == self.UP:
+            return "U"
+        elif d == self.DOWN:
+            return "D"
+        elif d == self.LEFT:
+            return "L"
+        elif d== self.RIGHT:
+            return "R"
+        else:
+            return "x"
+
+    def findStartPoint(self):
+        """
+        Find the first location in our array that is not empty
+        """
+        for i, row in enumerate(self.data):
+            for j, col in enumerate(row):
+                if self.data[i, j] != 0:  # or not np.isfinite(self.data[i,j]):
+                    return i, j
+        return None
+
+    def step(self, x, y):
+        """
+        Move from the current location to the next
+        """
+        upLeft = self.solid(x - 1, y - 1)
+        upRight = self.solid(x, y - 1)
+        downLeft = self.solid(x - 1, y)
+        downRight = self.solid(x, y)
+
+        state = 0
+        self.prev = self.next
+        # which cells are filled?
+        if upLeft:
+            state |= 1
+        if upRight:
+            state |= 2
+        if downLeft:
+            state |= 4
+        if downRight:
+            state |= 8
+
+        #what is the next step?
+        if state in [1, 5, 13]:
+            self.next = self.UP
+        elif state in [2, 3, 7]:
+            self.next = self.RIGHT
+        elif state in [4, 12, 14]:
+            self.next = self.LEFT
+        elif state in [8, 10, 11]:
+            self.next = self.DOWN
+        elif state == 6:
+            if self.prev == self.UP:
+                self.next = self.LEFT
+            else:
+                self.next = self.RIGHT
+        elif state == 9:
+            if self.prev == self.RIGHT:
+                self.next = self.UP
+            else:
+                self.next = self.DOWN
+        else:
+            self.next = self.NOWHERE
+        return
+
+    def solid(self, x, y):
+        """
+        Determine whether the pixel x,y is nonzero
+        """
+        if x < 0 or y < 0 or x >= self.xsize or y >= self.ysize:
+            return False
+        if self.data[x, y] == 0:
+            return False
+        if not np.isfinite(self.data[x, y]):
+            return False
+        return True
+
+    def walkPerimeter(self, startx, starty):
+        """
+        """
+        # checks
+        startx = max(startx, 0)
+        startx = min(startx, self.xsize)
+        starty = max(starty, 0)
+        starty = min(starty, self.ysize)
+
+        points = []
+
+        x, y = startx, starty
+
+        while True:
+            self.step(x, y)
+            if 0 <= x <= self.xsize and 0 <= y <= self.ysize:
+                points.append((x, y))
+            if self.next == self.UP:
+                y -= 1
+            elif self.next == self.LEFT:
+                x -= 1
+            elif self.next == self.DOWN:
+                y += 1
+            elif self.next == self.RIGHT:
+                x += 1
+            elif self.next == self.NOWHERE:
+                break
+            else:
+                #not sure what to do here
+                logging.warn("Failed to determine next step")
+                break
+            #stop when we return to the starting location
+            if x == startx and y == starty:
+                break
+            #print self.dir2str(self.next),
+
+        print 
+        return points
+
+    def doMarch(self):
+        """
+        March about and trace the outline of our object
+        """
+        x, y = self.findStartPoint()
+        perimeter = self.walkPerimeter(x, y)
+        return perimeter
+
+
+    def _blankWithin(self, perimeter):
+        """
+        Blank all the pixels within the given perimeter.
+        :param perimeter:
+        :return None:
+        """
+        def classify_next(cur,nxt):
+            result = self.NOWHERE
+            if cur[0] - nxt[0] < 0:
+                result = self.RIGHT
+            elif cur[0] - nxt[0] > 0:
+                result = self.LEFT
+            else:
+                if cur[1] - nxt[1] < 0:
+                    result = self.DOWN
+                elif cur[1] - nxt[1] > 0:
+                    result = self.UP
+                else:
+                    result = self.NOWHERE
+            return result
+
+        # Method:
+        # perimeter starts at the top-left most pixel of a region
+        # so we can 'scan right' from each 'DOWN' pixel in the perimeter, blanking as we go
+        # stop when we reach an already blanked pixel
+        # This will not work if there are 'inclusions' within the region.
+
+        # include the first pixel as the last pixel so we have a complete circuit.
+        circuit = perimeter+[perimeter[0]]
+        for i, p in enumerate(circuit[:-1]):
+            # figure out which direction we are going to be moving in
+            drn = classify_next(p, circuit[i+1])
+            if drn == self.DOWN:
+                for j in xrange(p[0], self.xsize):
+                    q = j, p[1]
+                    if self.data[q] == 0:  # stop when we reach an already blank pixel
+                        break
+                    else:
+                        self.data[q] = 0
+            elif drn == self.RIGHT:  # we don't blank right from a "RIGHT" pixel, we just blank that pixel.
+                if p[1] < self.ysize:  # look out for the edge of the data
+                    self.data[p] = 0
+        return
+
+    def doMarchAll(self):
+        """
+        Recursive march in the case that we have a fragmented shape
+        """
+        # copy the data since we are going to be modifying it
+        data_copy = copy(self.data)
+        
+        # iterate through finding an island, creating a perimeter,
+        # and then blanking the island
+        perimeters=[]
         p = self.findStartPoint()
         while p is not None:
-            x, y = self.findStartPoint()
+            x, y = p
             perim = self.walkPerimeter(x, y)
-            perimeter.extend(perim)
-            p = None
-            # For fragmented islands [Doesn't work]
-            # x,y = zip(*[p for p in perim if p[0]<self.xsize and p[1] < self.ysize])
-            # self.data[x,y] = 0
-            # p = self.findStartPoint()
-        return perimeter
+            perimeters.append(perim)
+            self._blankWithin(perim)
+            p = self.findStartPoint()
+        # restore the data 
+        self.data = data_copy
+        return perimeters
+                
 
 
 if __name__ == '__main__':
