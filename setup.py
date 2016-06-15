8fef4b4efa2934ebe04ea513f33c4c00da3af134
PaulHancock <paul.hancock@curtin.edu.au>
2016-06-15 13:47:40 +0800

    update version and formatting

--- setup.py
+++ setup.py
@@ -5,26 +5,30 @@ from setuptools import setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 def get_version():
-    return "1.9.6"
+    return "1.9.7-58-g02454c8"
+
 
 setup(
-    name = "AegeanTools",
-    version = get_version(),
-    author = "Paul Hancock",
-    author_email = "Mr.Paul.Hancock@gmail.com",
-    description = (""),
-#    license = "BSD",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/an_example_pypi_project",
+    name="AegeanTools",
+    version=get_version(),
+    author="Paul Hancock",
+    author_email="Mr.Paul.Hancock@gmail.com",
+    description=(""),
+    # license = "BSD",
+    keywords="example documentation tutorial",
+    url="http://packages.python.org/an_example_pypi_project",
     long_description=read('README.md'),
-    packages = ['AegeanTools'],
+    packages=['AegeanTools'],
     install_requires=['numpy>=1.10',
                       'scipy>=0.16',
                       'astropy>=1.0',
                       'lmfit>=0.9.2'],
-    scripts=['scripts/aegean','scripts/BANE','scripts/SR6','scripts/AeRes','scripts/MIMAS']
+    scripts=['scripts/aegean', 'scripts/BANE', 'scripts/SR6', 'scripts/AeRes', 'scripts/MIMAS']
 )
