c58e548e98b337c9c1d3bd8bb754cbfebe6d13a5
PaulHancock <paul.hancock@curtin.edu.au>
2017-08-23 20:56:16 +0800

    one test per func

--- tests/test_01.py
+++ tests/test_01.py
@@ -8,8 +8,11 @@ __author__ = 'Paul Hancock'
 __date__ = ''
 
 
-if __name__ == "__main__":
+def test_sigmaclip():
     data = np.random.random(100)
     print("TESTING BANE.sigmaclip")
     BANE.sigmaclip(data, 3, 4, reps=4)
     print("Pass")
+
+if __name__ == "__main__":
+    test_sigmaclip()
