7874823833fe978603c3b8babfaa6a6b63190a64
PaulHancock <paul.hancock@curtin.edu.au>
2017-08-23 21:26:06 +0800

    add test for fits_interp

--- tests/test_01.py
+++ tests/test_01.py
@@ -2,10 +2,12 @@
 from __future__ import print_function
 
 from AegeanTools import BANE
+from AegeanTools import fits_interp
 import numpy as np
+import os
 
 __author__ = 'Paul Hancock'
-__date__ = ''
+__date__ = '23/08/2017'
 
 
 def test_sigmaclip():
@@ -13,6 +15,16 @@ def test_sigmaclip():
     print("TESTING BANE.sigmaclip")
     BANE.sigmaclip(data, 3, 4, reps=4)
     print("Pass")
+
+
+def test_fits_interp_compress_then_expand():
+    # test compression and expansion
+    fits_interp.compress("Test/Images/1904-66_AIT.fits", factor=7, outfile='test.fits')
+    fits_interp.expand('test.fits', outfile='test2.fits', method='linear')
+    # cleanup
+    os.remove('test.fits')
+    os.remove('test2.fits')
+
 
 if __name__ == "__main__":
     test_sigmaclip()
