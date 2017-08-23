289b6d0f6bcab5800a988f9736d69a07f6653901
PaulHancock <paul.hancock@curtin.edu.au>
2017-08-23 21:57:28 +0800

    add tests: optimum_secions, mask_data

--- tests/test_01.py
+++ tests/test_01.py
@@ -10,6 +10,10 @@ __author__ = 'Paul Hancock'
 __date__ = '23/08/2017'
 
 
+###
+# BANE.py
+###
+
 def test_sigmaclip():
     # normal usage case
     data = np.random.random(100)
@@ -23,6 +27,25 @@ def test_sigmaclip():
     assert len(BANE.sigmaclip([], 0, 3)) == 0
 
 
+def test_optimum_sections():
+    # typical case
+    assert BANE.optimum_sections(8, (64, 64)) == (2, 4)
+    # redundant case
+    assert BANE.optimum_sections(1, (134, 1200)) == (1, 1)
+
+
+def test_mask_data():
+    data = np.ones((10, 10), dtype=np.float32)
+    mask = data.copy()
+    mask[3:5, 0:2] = np.nan
+    BANE.mask_img(data, mask)
+    # check that the nan regions overlap
+    assert np.all(np.isnan(data) == np.isnan(mask))
+
+###
+# fits_interp.py
+###
+
 def test_fits_interp_compress_then_expand():
     # test compression and expansion
     fits_interp.compress("Test/Images/1904-66_AIT.fits", factor=7, outfile='test.fits')
@@ -30,6 +53,9 @@ def test_fits_interp_compress_then_expand():
     # cleanup
     os.remove('test.fits')
     os.remove('test2.fits')
+
+
+
 
 
 if __name__ == "__main__":
