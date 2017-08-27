4557e9b360c3d7110eeeff9760c532686f9152fe
PaulHancock <paul.hancock@curtin.edu.au>
2017-08-27 13:34:06 +0800

    remove unused flags for flood fill

--- AegeanTools/flags.py
+++ AegeanTools/flags.py
@@ -1,8 +1,3 @@
-# Set some bitwise logic for flood routines
-PEAKED = 1    # 001
-QUEUED = 2    # 010
-VISITED = 4   # 100
-
 # Err Flags for fitting routines
 FITERRSMALL   = 1 # 00001
 FITERR        = 2 # 00010
