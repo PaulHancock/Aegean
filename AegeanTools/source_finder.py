70f242255dfda9ca81e3a6667fdf4f16c2a56172
PaulHancock <paul.hancock@curtin.edu.au>
2017-08-27 20:06:32 +0800

    fix log error

--- AegeanTools/source_finder.py
+++ AegeanTools/source_finder.py
@@ -1674,7 +1674,7 @@ def check_cores(cores):
     """
     cores = min(multiprocessing.cpu_count(), cores)
     if six.PY3:
-        log.info("Multi-cores not supported in python 3+, using one core")
+        logging.info("Multi-cores not supported in python 3+, using one core")
         return 1
     try:
         queue = pprocess.Queue(limit=cores, reuse=1)
