7abef330faba388af37ed28d6bac028c2c60a081
PaulHancock <paul.hancock@curtin.edu.au>
2017-09-02 12:38:49 +0800

    add test for image_filter

--- tests/test_BANE.py
+++ tests/test_BANE.py
@@ -3,11 +3,16 @@ from __future__ import print_function
 
 from AegeanTools import BANE
 from AegeanTools import fits_interp
+from astropy.io import fits
 import numpy as np
 import os
 
 __author__ = 'Paul Hancock'
 __date__ = '23/08/2017'
+
+import logging
+logging.basicConfig(format="%(module)s:%(levelname)s %(message)s")
+log = logging.getLogger("Aegean")
 
 def test_sigmaclip():
     # normal usage case
@@ -37,6 +42,25 @@ def test_mask_data():
     # check that the nan regions overlap
     assert np.all(np.isnan(data) == np.isnan(mask))
 
+
+def test_filter_image():
+    # data = np.random.random((30, 30), dtype=np.float32)
+    fname = 'tests/test_files/1904-66_SIN.fits'
+    outbase = 'dlme'
+    rms = outbase + '_rms.fits'
+    bkg = outbase + '_bkg.fits'
+    # hdu = fits.getheader(fname)
+    # shape = hdu[0]['NAXIS1'], hdu[0]['NAXIS2']
+    BANE.filter_image(fname, step_size=[10, 10], box_size=[100, 100], cores=1, out_base=outbase)
+    assert os.path.exists(rms)
+    os.remove(rms)
+    assert os.path.exists(bkg)
+    os.remove(bkg)
+    BANE.filter_image(fname, cores=2, out_base=outbase, twopass=True, compressed=True)
+    assert os.path.exists(rms)
+    os.remove(rms)
+    assert os.path.exists(bkg)
+    os.remove(bkg)
 
 if __name__ == "__main__":
     # introspect and run all the functions starting with 'test'
