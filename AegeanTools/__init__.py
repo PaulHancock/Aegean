d6949843502bdebe5e4fb4da236427874ce73c49
PaulHancock <paul.hancock@curtin.edu.au>
2017-12-28 12:15:16 +1100

    add doc string

--- AegeanTools/__init__.py
+++ AegeanTools/__init__.py
@@ -1,6 +1,14 @@
 #! /usr/bin/env python
-""" """
+"""
+-------------------
+AegeanTools module.
+-------------------
 
+This module was written primarily for the Aegean source finder, and the tools BANE and MIMAS.
+The wcs_helpers and angle_tools modules contain many useful classes and methods that would be useful more
+generally.
+
+"""
 __author__ = 'Paul Hancock'
 __version__ = '2.0b345'
 __date__ = '2017-12-12'
