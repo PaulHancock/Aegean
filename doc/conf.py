bea6617d555813b68c933c195f52fd9f8d809973
PaulHancock <paul.hancock@curtin.edu.au>
2018-01-10 15:23:45 +0800

    tell numpydoc to not do autosummary

--- doc/conf.py
+++ doc/conf.py
@@ -35,6 +35,9 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'numpydoc']
 
+# config numpydoc to not use autosummary
+numpydoc_show_class_members = False
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
