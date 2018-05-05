2fbeebb1d41f0186dabeb2f77e40aa9fe9196045
PaulHancock <paul.hancock@curtin.edu.au>
2018-05-05 10:54:35 +0800

    add comments to priorized fitting

--- AegeanTools/source_finder.py
+++ AegeanTools/source_finder.py
@@ -1648,21 +1648,29 @@ class SourceFinder(object):
 
         # the input sources are the initial conditions for our fits.
         # Expand each source size if needed.
+
+        # If ratio is provided we just the psf by this amount
         if ratio is not None:
             self.log.info("Using ratio of {0} to scale input source shapes".format(ratio))
             far *= ratio
             for i, src in enumerate(input_sources):
+                # Sources with an unknown psf are rejected as they are either outside the image
+                # or outside the region covered by the psf
                 skybeam = global_data.psfhelper.get_beam(src.ra, src.dec)
                 if skybeam is None:
                     src_mask[i] = False
                     self.log.info("Excluding source ({0.island},{0.source}) due to lack of psf knowledge".format(src))
                     continue
+                # the new source size is the previous size, convolved with the expanded psf
                 src.a = np.sqrt(src.a ** 2 + (skybeam.a * 3600) ** 2 * (1 - 1 / ratio ** 2))
                 src.b = np.sqrt(src.b ** 2 + (skybeam.b * 3600) ** 2 * (1 - 1 / ratio ** 2))
                 # source with funky a/b are also rejected
                 if not np.all(np.isfinite((src.a, src.b))):
                     self.log.info("Excluding source ({0.island},{0.source}) due to funky psf ({0.a},{0.b},{0.pa})".format(src))
                     src_mask[i] = False
+
+        # if we know the psf from the input catalogue (has_psf), or if it was provided via a psf map
+        # then we use that psf.
         elif catpsf is not None or has_psf:
             if catpsf is not None:
                 self.log.info("Using catalog PSF from {0}".format(catpsf))
@@ -1672,6 +1680,7 @@ class SourceFinder(object):
                 psf_helper = None
             for i, src in enumerate(input_sources):
                 if has_psf:
+                    # this should be src.psf_a, not src.a ! (etc)
                     catbeam = Beam(src.a * 3600, src.b * 3600, src.pa)
                 else:
                     catbeam = psf_helper.get_beam(src.ra, src.dec)
@@ -1681,7 +1690,14 @@ class SourceFinder(object):
                     src_mask[i] = False
                     self.log.info("Excluding source ({0.island},{0.source}) due to lack of psf knowledge".format(src))
                     continue
+
+                # TODO: The following assumes that the various psf's are scaled versions of each other
+                # and makes no account for differing position angles. This needs to be checked and/or addressed.
+
+                # deconvolve the source shape from the catalogue psf
                 src.a = (src.a / 3600) ** 2 - catbeam.a ** 2 + imbeam.a ** 2  # degrees
+
+                # clip the minimum source shape to be the image psf
                 if src.a < 0:
                     src.a = imbeam.a * 3600  # arcsec
                 else:
