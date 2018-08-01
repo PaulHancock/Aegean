c3f13b5134955d34d6d6dfff336b579df14b613d
PaulHancock <paul.hancock@curtin.edu.au>
2018-08-01 22:06:30 +0800

    remove code related to LinearNDInterpolator

--- AegeanTools/BANE.py
+++ AegeanTools/BANE.py
@@ -12,7 +12,6 @@ import multiprocessing
 import multiprocessing.sharedctypes
 import numpy as np
 import os
-from scipy.interpolate import LinearNDInterpolator
 from scipy.interpolate import RegularGridInterpolator
 import sys
 from time import gmtime, strftime
@@ -176,47 +175,24 @@ def sigma_filter(filename, region, step_size, box_size, shape, sid):
 
     vals = np.zeros(shape=(len(rows),len(cols)))
 
-    # lists that hold the calculated values and array coordinates
-    bkg_points = []
-    bkg_values = []
-    rms_points = []
-    rms_values = []
-
     for i, row in enumerate(rows):
         for j, col in enumerate(cols):
             r_min, r_max, c_min, c_max = box(row, col)
             new = data[r_min:r_max, c_min:c_max]
             new = np.ravel(new)
             new, bkg, _ = sigmaclip(new, 3, 3)
-            # If we are left with (or started with) no data, then just move on
             if len(new) < 1:
                 continue
-            # these coords need to be indices into the larger array
-            bkg_points.append((row + data_row_min, col))
-            bkg_values.append(bkg)
             vals[i,j] = bkg
 
     # indices of the shape we want to write to (not the shape of data)
-    gx, gy = np.mgrid[ymin:ymax, 0:shape[1]]
     gr, gc = np.mgrid[ymin-data_row_min:ymax-data_row_min, 0:shape[1]]
 
-    if len(bkg_points) > 1:
-        logging.debug("Interpolating bkg")
-        # ifunc = LinearNDInterpolator(bkg_points, bkg_values)
-        ifunc = RegularGridInterpolator((rows,cols), vals)
-        try:
-            interpolated_bkg = np.array(ifunc((gr, gc)), dtype=np.float32)
-        except ValueError as e:
-            logging.debug("rows {0}".format(rows))
-            logging.debug("cols {0}".format(cols))
-            logging.debug("vals {0}".format(vals))
-            logging.debug("gr {0}".format(gr))
-            logging.debug("gc {0}".format(gc))
-            raise e
-        del ifunc
-    else:
-        logging.debug("bkg is all nans")
-        interpolated_bkg = np.empty(gx.shape, dtype=np.float32)*np.nan
+    logging.debug("Interpolating bkg")
+    ifunc = RegularGridInterpolator((rows,cols), vals)
+    interpolated_bkg = np.array(ifunc((gr, gc)), dtype=np.float32)
+    del ifunc
+
     with ibkg.get_lock():
         logging.debug("Writing bkg to sharemem")
         for i, row in enumerate(interpolated_bkg):
@@ -243,33 +219,15 @@ def sigma_filter(filename, region, step_size, box_size, shape, sid):
             new = data[r_min:r_max, c_min:c_max]
             new = np.ravel(new)
             new, _ , rms = sigmaclip(new, 3, 3)
-            # If we are left with (or started with) no data, then just move on
             if len(new) < 1:
                 continue
-
-            rms_points.append((row + data_row_min, col))
-            rms_values.append(rms)
             vals[i,j] = rms
 
     # If the rms calculation above didn't yield any points, then our interpolated values are all nans
-    if len(rms_points) > 1:
-        logging.debug("Interpolating rms")
-        #ifunc = LinearNDInterpolator(rms_points, rms_values)
-        # force 32 bit floats
-        ifunc = RegularGridInterpolator((rows,cols), vals)
-        try:
-            interpolated_rms = np.array(ifunc((gr, gc)), dtype=np.float32)
-        except ValueError as e:
-            logging.debug("rows {0}".format(rows))
-            logging.debug("cols {0}".format(cols))
-            logging.debug("vals {0}".format(vals))
-            logging.debug("gr {0}".format(gr))
-            logging.debug("gc {0}".format(gc))
-            raise e
-        del ifunc
-    else:
-        logging.debug("rms is all nans")
-        interpolated_rms = np.empty(gx.shape, dtype=np.float32)*np.nan
+    logging.debug("Interpolating rms")
+    ifunc = RegularGridInterpolator((rows,cols), vals)
+    interpolated_rms = np.array(ifunc((gr, gc)), dtype=np.float32)
+    del ifunc
 
     with irms.get_lock():
         logging.debug("Writing rms to sharemem")
