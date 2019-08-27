remove calculation of projected beam size - fixes larger-than-expected integrated flux densities

AegeanTools/wcs_helpers.py
@@ -613,10 +613,10 @@ class PSFHelper(WCSHelper):
         area : float
             The area of the beam in square pixels.
         """
-        beam = self.get_pixbeam(ra, dec)
-        if beam is None:
-            return 0
-        return beam.a * beam.b * np.pi
+
+	parea = abs(self.wcshelper.pixscale[0] * self.wcshelper.pixscale[1])  # in deg**2 at reference coords
+        barea = self.get_beamarea_deg2(ra, dec)
+        return barea / parea
 
     def get_beamarea_deg2(self, ra, dec):
 