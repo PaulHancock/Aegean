fix bug where seed_clip was used instead of flood_clip in the calculation of islands for fitting.

AegeanTools/source_finder.py
@@ -106,7 +106,7 @@ def find_islands(im, bkg, rms,
         if np.any(snr[xmin:xmax, ymin:ymax] > seed_clip):  # obey seed clip constraint
             data_box = copy.copy(im[xmin:xmax, ymin:ymax])  # copy so that we don't blank the master data
             data_box[np.where(
-                snr[xmin:xmax, ymin:ymax] < seed_clip)] = np.nan  # blank pixels that are outside the outerclip
+            snr[xmin:xmax, ymin:ymax] < flood_clip)] = np.nan  # blank pixels that are outside the outerclip
             data_box[np.where(l[xmin:xmax, ymin:ymax] != i + 1)] = np.nan  # blank out other summits
             # check if there are any pixels left unmasked
             if not np.any(np.isfinite(data_box)):