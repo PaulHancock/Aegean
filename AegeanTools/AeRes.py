ensure that masking is applied as intended fixes issue #131

AegeanTools/AeRes.py
@@ -121,12 +121,9 @@ def make_model(sources, shape, wcshelper, mask=False, frac=None, sigma=4):
             logging.debug(" flux, sx, sy: {0} {1} {2}".format(src.peak_flux, sx, sy))
 
         # positions for which we want to make the model
-        x, y = np.mgrid[xmin:xmax, ymin:ymax]
-        x = list(map(int, x.ravel()))
-        y = list(map(int, y.ravel()))
-        # x, y = np.mgrid[int(xmin):int(xmax), int(ymin):int(ymax)]
-        # x = x.ravel()
-        # y = y.ravel()
+        x, y = np.mgrid[int(xmin):int(xmax), int(ymin):int(ymax)]
+        x = x.ravel()
+        y = y.ravel()
 
         # TODO: understand why xo/yo -1 is needed
         model = fitting.elliptical_gaussian(x, y, src.peak_flux, xo-1, yo-1, sx*FWHM2CC, sy*FWHM2CC, theta)
@@ -137,15 +134,12 @@ def make_model(sources, shape, wcshelper, mask=False, frac=None, sigma=4):
                 indices = np.where(model >= (frac*src.peak_flux))
             else:
                 indices = np.where(model >= (sigma*src.local_rms))
-            model[indices] = np.nan
-
-        m[x, y] += model
-        #     # somehow m[x,y][indices] = np.nan doesn't assign any values
-        #     # so we have to do the more complicated
-        #     # m[x[indices],y[indices]] = np.nan
-        #     m[x[indices], y[indices]]= np.nan
-        # else:
-        #     m[x, y] += model
+            # somehow m[x,y][indices] = np.nan doesn't assign any values
+            # so we have to do the more complicated
+            # m[x[indices],y[indices]] = np.nan
+            m[x[indices], y[indices]]= np.nan
+        else:
+            m[x, y] += model
         i_count += 1
     logging.info("modeled {0} sources".format(i_count))
     return m