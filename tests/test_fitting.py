9ecbad08dbecff2ac41fcd31639f027769167aa7
PaulHancock <paul.hancock@curtin.edu.au>
2021-11-15 14:06:16 +0800

    pep8 and small tidyup

--- tests/test_fitting.py
+++ tests/test_fitting.py
@@ -28,19 +28,25 @@ def make_model():
 def test_elliptical_gaussian():
     """Test our elliptical gaussian creation function"""
     x, y = np.indices((3, 3))
-    gauss = fitting.elliptical_gaussian(x, y, amp=1, xo=0, yo=1, sx=1, sy=1, theta=0)
-    if np.any(np.isnan(gauss)): raise AssertionError()
-    gauss = fitting.elliptical_gaussian(x, y, amp=1, xo=0, yo=1, sx=1, sy=1, theta=np.inf)
-    if not (np.all(np.isnan(gauss))): raise AssertionError()
+    gauss = fitting.elliptical_gaussian(
+        x, y, amp=1, xo=0, yo=1, sx=1, sy=1, theta=0)
+    if np.any(np.isnan(gauss)):
+        raise AssertionError()
+    gauss = fitting.elliptical_gaussian(
+        x, y, amp=1, xo=0, yo=1, sx=1, sy=1, theta=np.inf)
+    if not (np.all(np.isnan(gauss))):
+        raise AssertionError()
 
 
 def test_CandBmatrix():
     """Test that C and B matricies can be created without error"""
     x, y = map(np.ravel, np.indices((3, 3)))
     C = fitting.Cmatrix(x, y, sx=1, sy=2, theta=0)
-    if np.any(np.isnan(C)): raise AssertionError()
+    if np.any(np.isnan(C)):
+        raise AssertionError()
     B = fitting.Bmatrix(C)
-    if np.any(np.isnan(B)): raise AssertionError()
+    if np.any(np.isnan(B)):
+        raise AssertionError()
 
 
 def test_hessian_shape():
@@ -50,7 +56,8 @@ def test_hessian_shape():
     nvar = 3
     x, y = np.indices((10, 10))
     Hij = fitting.hessian(model, x, y)
-    if not (Hij.shape == (nvar, nvar, 10, 10)): raise AssertionError()
+    if not (Hij.shape == (nvar, nvar, 10, 10)):
+        raise AssertionError()
 
     # now add another component
     model.add('c1_amp', 1, vary=True)
@@ -62,7 +69,8 @@ def test_hessian_shape():
     nvar = 9
     model['components'].value = 2
     Hij = fitting.hessian(model, x, y)
-    if not (Hij.shape == (nvar, nvar, 10, 10)): raise AssertionError()
+    if not (Hij.shape == (nvar, nvar, 10, 10)):
+        raise AssertionError()
 
 
 def test_jacobian_shape():
@@ -74,7 +82,8 @@ def test_jacobian_shape():
     nvar = 3
     x, y = np.indices((10, 10))
     Jij = fitting.jacobian(model, x, y)
-    if not (Jij.shape == (nvar, 10, 10)): raise AssertionError()
+    if not (Jij.shape == (nvar, 10, 10)):
+        raise AssertionError()
 
     model.add('c1_amp', 1, vary=True)
     model.add('c1_xo', 5, vary=True)
@@ -85,7 +94,8 @@ def test_jacobian_shape():
     nvar = 9
     model['components'].value = 2
     Jij = fitting.jacobian(model, x, y)
-    if not (Jij.shape == (nvar, 10, 10)): raise AssertionError()
+    if not (Jij.shape == (nvar, 10, 10)):
+        raise AssertionError()
 
 
 def test_emp_vs_ana_jacobian():
@@ -96,7 +106,8 @@ def test_emp_vs_ana_jacobian():
     emp_Jij = fitting.emp_jacobian(model, x, y)
     ana_Jij = fitting.jacobian(model, x, y)
     diff = np.abs(ana_Jij - emp_Jij)
-    if not (np.max(diff) < 1e-5): raise AssertionError()
+    if not (np.max(diff) < 1e-5):
+        raise AssertionError()
 
     model.add('c1_amp', 1, vary=True)
     model.add('c1_xo', 5, vary=True)
@@ -109,7 +120,8 @@ def test_emp_vs_ana_jacobian():
     emp_Jij = fitting.emp_jacobian(model, x, y)
     ana_Jij = fitting.jacobian(model, x, y)
     diff = np.abs(ana_Jij - emp_Jij)
-    if not (np.max(diff) < 1e-3): raise AssertionError()
+    if not (np.max(diff) < 1e-3):
+        raise AssertionError()
 
 
 def test_emp_vs_ana_hessian():
@@ -120,7 +132,8 @@ def test_emp_vs_ana_hessian():
     emp_Hij = fitting.emp_hessian(model, x, y)
     ana_Hij = fitting.hessian(model, x, y)
     diff = np.abs(ana_Hij - emp_Hij)
-    if not (np.max(diff) < 1e-5): raise AssertionError()
+    if not (np.max(diff) < 1e-5):
+        raise AssertionError()
 
     model.add('c1_amp', 1, vary=True)
     model.add('c1_xo', 5, vary=True)
@@ -133,17 +146,20 @@ def test_emp_vs_ana_hessian():
     emp_Hij = fitting.emp_hessian(model, x, y)
     ana_Hij = fitting.hessian(model, x, y)
     diff = np.abs(ana_Hij - emp_Hij)
-    if not (np.max(diff) < 1): raise AssertionError()
+    if not (np.max(diff) < 1):
+        raise AssertionError()
 
 
 def test_make_ita():
     """Test make_ita"""
     noise = np.random.random((10, 10))
     ita = fitting.make_ita(noise)
-    if not (ita.shape == (100, 100)): raise AssertionError()
+    if not (ita.shape == (100, 100)):
+        raise AssertionError()
     noise *= np.nan
     ita = fitting.make_ita(noise)
-    if not (len(ita) == 0): raise AssertionError()
+    if not (len(ita) == 0):
+        raise AssertionError()
 
 
 def test_RB_bias():
@@ -151,7 +167,8 @@ def test_RB_bias():
     data = np.random.random((4, 4))
     model = make_model()
     bias = fitting.RB_bias(data, model)
-    if not (len(bias) == 3): raise AssertionError()
+    if not (len(bias) == 3):
+        raise AssertionError()
 
 
 def test_bias_correct():
@@ -172,14 +189,23 @@ def test_condon_errs():
     source.local_rms = 0.1
     source.peak_flux = 1
     source.int_flux = 1
+
     fitting.condon_errors(source, None)
-    if not (source.err_a == 0): raise AssertionError()
+    if not (source.err_a == 0):
+        raise AssertionError(
+            "err_a should be zero but is {0}".format(source.err_a))
+
     fitting.condon_errors(source, theta_n=8.)
-    if not (source.err_a > 0): raise AssertionError()
+    if not (source.err_a > 0):
+        raise AssertionError("err_a should be non-zero")
+
     # test that we can get a PA error
     source.a = 20
     fitting.condon_errors(source, None)
+    if source.err_pa < 0:
+        raise AssertionError("err_pa<0")
     if source.err_pa < 0: raise AssertionError()
+
 
 if __name__ == "__main__":
     # introspect and run all the functions starting with 'test'
