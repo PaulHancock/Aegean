917055e69a9334b7ea5d6ae3f1570649cc8fb909
PaulHancock <paul.hancock@curtin.edu.au>
2021-11-29 15:42:32 +0800

    update date

--- AegeanTools/__init__.py
+++ AegeanTools/__init__.py
@@ -11,7 +11,7 @@ generally.
 """
 __author__ = 'Paul Hancock'
 __version__ = '2.2.5'
-__date__ = '2021-11-15'
+__date__ = '2021-11-29'
 __citation__ = """
 % If your work makes use of AegeanTools please cite the following papers as appropriate:
 
