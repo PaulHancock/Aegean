d15189a97608005b3109c07453b513f4fdbc5983
PaulHancock <paul.hancock@curtin.edu.au>
2022-08-11 10:22:35 +0800

    create build scripts for docker containers

---
+++ containers/docker_build.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+function usage {
+        echo "Usage: $(basename $0) [git branch name or commit hash]" 2>&1
+        exit 1
+}
+
+if [[ ${#} -eq 0 ]]; then
+   version='main';
+else
+    version=$1;
+fi
+
+echo "Building with branch/checkout: ${version}"
+# build the container and update tag
+docker build --build-arg gitargs="@${version}" . -f Dockerfile -t "paulhancock/aegean:${version}" && \
+echo "Run\nDocker push paulhancock/aegean:${version}"
