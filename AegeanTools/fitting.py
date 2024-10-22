32d969a2a207f7b097599a80df31683026385792
AhmedHamadto <Ahmed.Abdelmuniem@gmail.com>
2024-10-22 11:21:26 +0800

    undid the last commit.

--- AegeanTools/fitting.py
+++ AegeanTools/fitting.py
@@ -1238,7 +1238,7 @@ def ntwodgaussian_lmfit(params):
 
     return rfunc
 
-def do_lmfit(data, params, B=None, errs=None, dojac=False):
+def do_lmfit(data, params, B=None, errs=None, dojac=True):
     """
     Fit the model to the data
     data may contain 'flagged' or 'masked' data with the value of np.NaN
